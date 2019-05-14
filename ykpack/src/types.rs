ff6a32061867e0a26653c90415367295ef91d8a3
Edd Barrett <vext01@gmail.com>
2019-05-14 15:44:50 +0100

    First attempt at TIRv2.
    TIR needs to be a lean little language rather than a mirroring of MIR.
    
     * TIR is LLVM-IR-like.
     * TIR variables are typed, operations are not.

--- ykpack/src/types.rs
+++ ykpack/src/types.rs
@@ -16,6 +16,34 @@ pub type CrateHash = u64;
 pub type DefIndex = u32;
 pub type BasicBlockIndex = u32;
 pub type LocalIndex = u32;
+pub type TyIndex = u32;
+pub type FieldIndex = u32;
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Copy)]
+pub struct Local {
+    idx: LocalIndex,
+    ty: TyIndex,
+}
+
+impl Local {
+    pub fn new(idx: LocalIndex, ty: TyIndex) -> Self {
+        Self { idx, ty }
+    }
+
+    pub fn idx(&self) -> LocalIndex {
+        self.idx
+    }
+
+    pub fn ty(&self) -> TyIndex {
+        self.ty
+    }
+}
+
+impl Display for Local {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "${}: t{}", self.idx, self.ty)
+    }
+}
 
 /// A mirror of the compiler's notion of a "definition ID".
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
@@ -62,9 +90,12 @@ impl Display for Tir {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "[Begin TIR for {}]", self.item_path_str)?;
         writeln!(f, "    {}:", self.def_id)?;
+        let mut block_strs = Vec::new();
         for (i, b) in self.blocks.iter().enumerate() {
-            write!(f, "    bb{}:\n{}", i, b)?;
+            block_strs.push(format!("    bb{}:\n{}", i, b));
         }
+        println!("{:?}", block_strs);
+        writeln!(f, "{}", block_strs.join("\n"))?;
         writeln!(f, "[End TIR for {}]", self.item_path_str)?;
         Ok(())
     }
@@ -85,21 +116,76 @@ impl BasicBlock {
 impl Display for BasicBlock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for s in self.stmts.iter() {
-            write!(f, "        {}", s)?;
+            write!(f, "        {}\n", s)?;
         }
-        writeln!(f, "        term: {}\n", self.term)
+        write!(f, "        {}", self.term)
     }
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
 pub enum Statement {
+    /// Do nothing.
     Nop,
+    /// An assignment to a local variable.
+    Assign(Local, Rvalue),
+    /// Store into the memory.
+    Store(Local, Operand),
+    /// Any unimplemented lowering maps to this variant.
     Unimplemented,
 }
 
 impl Display for Statement {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "{:?}", self)
+        match self {
+            Statement::Nop => write!(f, "nop"),
+            Statement::Assign(l, r) => write!(f, "{} = {}", l, r),
+            Statement::Store(ptr, val) => write!(f, "store({}, {})", ptr, val),
+            _ => write!(f, "unimplemented"),
+        }
+    }
+}
+
+/// The right-hand side of an assignment.
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub enum Rvalue {
+    /// Another local variable.
+    Operand(Operand),
+    /// Get a pointer to a field.
+    GetField(Local, FieldIndex),
+    /// Load a value of specified type from a pointer.
+    Load(Local),
+    /// Binary Ops.
+    Add(Operand, Operand),
+    Sub(Operand, Operand),
+    /// Allocate space for the specified type on the stack and return a pointer to it.
+    Alloca(TyIndex),
+}
+
+impl Display for Rvalue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Rvalue::Operand(o) => write!(f, "{}", o),
+            Rvalue::GetField(ptr, fidx) => write!(f, "get_field({}, {})", ptr, fidx),
+            Rvalue::Load(l) => write!(f, "load({})", l),
+            Rvalue::Add(l, r) => write!(f, "int_add({}, {})", l, r),
+            Rvalue::Sub(l, r) => write!(f, "int_sub({}, {})", l, r),
+            Rvalue::Alloca(t) => write!(f, "alloca({})", t),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub enum Operand {
+    Local(Local),
+    Constant(Constant),
+}
+
+impl Display for Operand {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Operand::Local(l) => write!(f, "{}", l),
+            Operand::Constant(c) => write!(f, "{}", c),
+        }
     }
 }
 
@@ -110,6 +196,16 @@ pub enum Constant {
     Unimplemented,
 }
 
+impl Display for Constant {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Constant::UnsignedInt(u) => write!(f, "{}", u),
+            Constant::SignedInt(s) => write!(f, "{}", s),
+            Constant::Unimplemented => write!(f, "Unimplemented Constant"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
 pub enum UnsignedInt {
     Usize(usize),
@@ -118,7 +214,12 @@ pub enum UnsignedInt {
     U32(u32),
     U64(u64),
     U128 { hi: u64, lo: u64 },
-    Unimplemented,
+}
+
+impl Display for UnsignedInt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 impl UnsignedInt {
@@ -146,7 +247,12 @@ pub enum SignedInt {
     I32(i32),
     I64(i64),
     I128 { hi: u64, lo: u64 },
-    Unimplemented,
+}
+
+impl Display for SignedInt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 impl SignedInt {
