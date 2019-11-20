99171d4daf36a74d406cae14319bfec779d88abf
Laurence Tratt <laurie@tratt.net>
2019-11-20 20:50:13 +0000

    Move to our new licensing regime.
    All copyright in this project is held by King's, so moving it is trivial.

--- ykpack/src/lib.rs
+++ ykpack/src/lib.rs
@@ -1,12 +1,3 @@
-// Copyright 2019 King's College London.
-// Created by the Software Development Team <http://soft-dev.org/>.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! ykpack -- Serialiser and deserialiser for carrying data from compile-time to run-time.
 //!
 //! This crate allows ykrustc to serialise various compile-time information for later
--- yktrace/src/swt.rs
+++ yktrace/src/swt.rs
@@ -1,12 +1,3 @@
-// Copyright 2019 King's College London.
-// Created by the Software Development Team <http://soft-dev.org/>.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! Software tracing via ykrustc.
 
 use super::{SirTrace, ThreadTracer, ThreadTracerImpl};
