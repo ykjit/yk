1768bbc5118d96f4261309272fe9d05f8200877f
Lukas Diekmann <lukas.diekmann@gmail.com>
2020-03-19 12:20:02 +0000

    The path of rasm may change depending on the OS

--- assembler/src/backend/rasm.rs
+++ assembler/src/backend/rasm.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 pub fn encode(input: &str) -> Vec<u8> {
-    let output = Command::new("/usr/bin/rasm2")
+    let output = Command::new("rasm2")
         .arg("-B")
         .arg(input)
         .output()
