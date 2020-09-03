eb50f54ac82eb94a9c6625f788d5f46b77e5ec7c
bjorn3 <bjorn3@users.noreply.github.com>
2020-09-03 14:18:42 +0200

    Use bincode instead of msgpack for SIR serialization

--- ykpack/src/decode.rs
+++ ykpack/src/decode.rs
@@ -1,31 +1,25 @@
 use crate::Pack;
 use fallible_iterator::FallibleIterator;
-use rmp_serde::{
-    decode::{self, ReadReader},
-    Deserializer,
-};
-use serde::Deserialize;
 use std::io::Read;
 
 /// The pack decoder.
 /// Offers a simple iterator interface to serialised packs.
 pub struct Decoder<'a> {
-    deser: Deserializer<ReadReader<&'a mut dyn Read>>,
+    from: &'a mut dyn Read,
 }
 
 impl<'a> Decoder<'a> {
     /// Returns a new decoder which will deserialise from `read_from`.
     pub fn from(read_from: &'a mut dyn Read) -> Self {
-        let deser = Deserializer::new(read_from);
-        Self { deser }
+        Self { from: read_from }
     }
 }
 
 impl<'a> FallibleIterator for Decoder<'a> {
     type Item = Pack;
-    type Error = decode::Error;
+    type Error = bincode::Error;
 
     fn next(&mut self) -> Result<Option<Self::Item>, Self::Error> {
-        Option::<Pack>::deserialize(&mut self.deser)
+        bincode::deserialize_from(&mut *self.from)
     }
 }
