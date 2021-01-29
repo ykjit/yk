5ed4635163dcdfae83950d561f072f580cfb3cb9
Edd Barrett <vext01@gmail.com>
2021-01-29 16:00:37 +0000

    Add a comment about where Drop impls should live.

--- ykshim_client/src/lib.rs
+++ ykshim_client/src/lib.rs
@@ -2,6 +2,13 @@
 //!
 //! For more information, see this section in the documentation:
 //! https://softdevteam.github.io/ykdocs/tech/yk_structure.html
+//!
+//! Put anything used only in testing in the `test_api` module. Everything else should go in the
+//! `prod_api` module.
+//!
+//! The exception to this rule is `Drop` implementations for opaque pointer wrappers. These should
+//! always go in the `prod_api` module. It's hard to know all of the call sites for `drop()` since
+//! they are implicit, so let's assume the production API does use them.
 
 // FIXME handle all errors that may pass over the API boundary.
 
