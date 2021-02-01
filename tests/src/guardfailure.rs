c78c7c87f77253a2f3cac7f3e3b92315393630d1
Lukas Diekmann <lukas.diekmann@gmail.com>
2021-02-01 15:45:56 +0000

    Test recursive calls in the SIR interpreter.
    Requires implemeting binary operations in the SIR interpreter, so
    currently ignored.

--- tests/src/guardfailure.rs
+++ tests/src/guardfailure.rs
@@ -77,3 +77,41 @@ fn recursion() {
     unsafe { si.interpret(&mut args as *mut _ as *mut u8) };
     assert_eq!(args.1, 99);
 }
+
+#[ignore]
+#[test]
+fn recursion2() {
+    struct IO(u8, u8);
+
+    // Test that the SIR interpreter can deal with new recursions after a guard failure.
+    fn rec(i: u8, j: u8) -> u8 {
+        if i < 5 {
+            return rec(i + 1, j + 1);
+        } else {
+            return j;
+        }
+    }
+
+    #[interp_step]
+    fn interp_step(io: &mut IO) {
+        let x = rec(io.0, io.1);
+        io.1 = x;
+    }
+
+    let mut inputs = IO(7, 0);
+    let th = start_tracing(TracingKind::HardwareTracing);
+    interp_step(&mut inputs);
+    let sir_trace = th.stop_tracing().unwrap();
+    let ct = compile_trace(sir_trace).unwrap();
+    let mut args = IO(7, 1);
+    assert!(unsafe { ct.execute(&mut args).is_null() });
+    assert_eq!(args.1, 1);
+    // Execute trace with input that fails the guard.
+    let mut args = IO(1, 0);
+    let ptr = unsafe { ct.execute(&mut args) };
+    assert!(!ptr.is_null());
+    // Check that running the interpreter gets us the correct result.
+    let mut si: SIRInterpreter = SIRInterpreter(ptr);
+    unsafe { si.interpret(&mut args as *mut _ as *mut u8) };
+    assert_eq!(args.1, 5);
+}
