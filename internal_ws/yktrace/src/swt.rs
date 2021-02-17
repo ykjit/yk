a76df1ee80d27bbb405905b357df4283dacc3b50
bjorn3 <bjorn3@users.noreply.github.com>
2021-02-17 12:29:40 +0000

    Introduce the TraceBuffer abstraction to avoid a RefCell

--- internal_ws/yktrace/src/swt.rs
+++ internal_ws/yktrace/src/swt.rs
@@ -1,8 +1,9 @@
 //! Software tracing via ykrustc.
 
+use self::trace_buffer::TraceBuffer;
 use super::{SirTrace, ThreadTracer, ThreadTracerImpl};
 use crate::{errors::InvalidTraceError, sir::SirLoc};
-use std::cell::RefCell;
+use std::cell::UnsafeCell;
 
 /// Softare thread tracer.
 struct SWTThreadTracer;
@@ -21,18 +22,7 @@ impl ThreadTracerImpl for SWTThreadTracer {
 
         let locs = TRACE_BUF.with(|trace_buf| {
             // When we make a SirTrace, we convert all of the locations from SwtLoc to SirLoc.
-            trace_buf
-                .borrow_mut()
-                .drain(..)
-                .map(|swt_loc| {
-                    let symbol_name = unsafe { std::ffi::CStr::from_ptr(swt_loc.symbol_name) };
-                    SirLoc {
-                        symbol_name: symbol_name.to_str().unwrap(),
-                        bb_idx: swt_loc.bb_idx,
-                        addr: None
-                    }
-                })
-                .collect()
+            trace_buf.get_sir_locs_and_clear()
         });
 
         Ok(SirTrace::new(locs))
@@ -41,7 +31,7 @@ impl ThreadTracerImpl for SWTThreadTracer {
 
 pub(crate) fn start_tracing() -> ThreadTracer {
     TRACE_BUF.with(|trace_buf| {
-        assert!(trace_buf.borrow().is_empty());
+        assert!(trace_buf.is_empty());
     });
 
     unsafe {
@@ -63,7 +53,7 @@ struct SwtLoc {
 
 thread_local! {
     /// The trace buffer.
-    static TRACE_BUF: RefCell<Vec<SwtLoc>> = RefCell::new(Vec::with_capacity(1024));
+    static TRACE_BUF: TraceBuffer = TraceBuffer::new();
 }
 
 /// true = we are tracing, false = we are not tracing or an error occurred.
@@ -130,9 +120,54 @@ __yk_swt_rec_loc_active:
 #[no_mangle]
 unsafe extern "C" fn __yk_swt_rec_loc_impl(symbol_name: *const i8, bb_idx: u32) {
     TRACE_BUF.with(|trace_buf| {
-        trace_buf.borrow_mut().push(SwtLoc {
+        trace_buf.push(SwtLoc {
             symbol_name,
             bb_idx
         });
     });
+}
+
+mod trace_buffer {
+    use super::*;
+
+    /// A buffer containing [`SwtLoc`]s. All public methods only require an immutable reference,
+    /// thereby allowing it to be stored inside a thread local without the overhead of a `RefCell`.
+    pub(super) struct TraceBuffer(UnsafeCell<Vec<SwtLoc>>);
+
+    impl TraceBuffer {
+        pub(super) fn new() -> Self {
+            TraceBuffer(UnsafeCell::new(Vec::with_capacity(1024)))
+        }
+
+        pub(super) fn is_empty(&self) -> bool {
+            // SAFETY: The api of `TraceBuffer` prevents any mutable references for the duration of
+            // this call.
+            unsafe { (&*self.0.get()).is_empty() }
+        }
+
+        #[inline]
+        pub(super) fn push(&self, loc: SwtLoc) {
+            // SAFETY: The api of `TraceBuffer` prevents any other references for the duration of
+            // this call.
+            unsafe {
+                (&mut *self.0.get()).push(loc);
+            }
+        }
+
+        pub(super) fn get_sir_locs_and_clear(&self) -> Vec<SirLoc> {
+            // SAFETY: The api of `TraceBuffer` prevents any other references for the duration of
+            // this call.
+            unsafe { &mut *self.0.get() }
+                .drain(..)
+                .map(|swt_loc| {
+                    let symbol_name = unsafe { std::ffi::CStr::from_ptr(swt_loc.symbol_name) };
+                    SirLoc {
+                        symbol_name: symbol_name.to_str().unwrap(),
+                        bb_idx: swt_loc.bb_idx,
+                        addr: None
+                    }
+                })
+                .collect()
+        }
+    }
 }
