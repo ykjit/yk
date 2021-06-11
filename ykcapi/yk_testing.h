4fd80ca0415bd73aaa5bb9a99f60450aa5d8d2db
Lukas Diekmann <lukas.diekmann@gmail.com>
2021-06-11 14:51:10 +0100

    Clarify how we pass vars into a compiled trace.

--- ykcapi/yk_testing.h
+++ ykcapi/yk_testing.h
@@ -9,6 +9,10 @@ void *__yktrace_hwt_mapper_blockmap_new(void);
 size_t __yktrace_hwt_mapper_blockmap_len(void *mapper);
 void __yktrace_hwt_mapper_blockmap_free(void *mapper);
 
+// Until we have a proper API for tracing, variables that we want to pass into
+// a compiled trace need to be "registered" by passing them into
+// __yktrace_start_tracing. While the start tracing call ignores them, it
+// allows us identify them when preparing the inlined trace code.
 void *__yktrace_start_tracing(uintptr_t kind, ...);
 void *__yktrace_stop_tracing(void *tt);
 size_t __yktrace_irtrace_len(void *trace);
