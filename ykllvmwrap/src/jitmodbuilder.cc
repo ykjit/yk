ce7afd4e91e5fc333d698588e21bec7e7de31a1e
Lukas Diekmann <lukas.diekmann@gmail.com>
2021-06-29 15:07:36 +0100

    Move code that builds JIT module into class.

---
+++ ykllvmwrap/src/jitmodbuilder.cc
@@ -0,0 +1,301 @@
+// Classes and functions for constructing a new LLVM module from a trace.
+
+using namespace llvm;
+using namespace std;
+
+// An atomic counter used to issue compiled traces with unique names.
+atomic<uint64_t> NextTraceIdx(0);
+
+#define TRACE_FUNC_PREFIX "__yk_compiled_trace_"
+#define YKTRACE_START "__yktrace_start_tracing"
+#define YKTRACE_STOP "__yktrace_stop_tracing"
+
+std::vector<Value *> getTraceInputs(Function *F, uintptr_t BBIdx) {
+  std::vector<Value *> Vec;
+  auto It = F->begin();
+  // Skip to the first block in the trace which contains the `start_tracing`
+  // call.
+  std::advance(It, BBIdx);
+  BasicBlock *BB = &*It;
+  bool found = false;
+  for (auto I = BB->begin(); I != BB->end(); I++) {
+    if (isa<CallInst>(I)) {
+      CallInst *CI = cast<CallInst>(&*I);
+      if (CI->getCalledFunction()->getName() == YKTRACE_START) {
+        // Skip first argument to start_tracing.
+        for (auto Arg = CI->arg_begin() + 1; Arg != CI->arg_end(); Arg++) {
+          Vec.push_back(Arg->get());
+        }
+        found = true;
+        break;
+      }
+    }
+  }
+  if (!found)
+    errx(EXIT_FAILURE, "failed to find trace inputs");
+  return Vec;
+}
+
+class JITModBuilder {
+public:
+  ValueToValueMapTy VMap;
+#ifndef NDEBUG
+  ValueToValueMapTy RevVMap;
+#endif
+  string TraceName;
+
+  Module *createModule(char *FuncNames[], size_t BBs[], size_t Len,
+                       Module *AOTMod) {
+    LLVMContext &JITContext = AOTMod->getContext();
+    auto JITMod = new Module("", JITContext);
+    uint64_t TraceIdx = NextTraceIdx.fetch_add(1);
+    if (TraceIdx == numeric_limits<uint64_t>::max())
+      errx(EXIT_FAILURE, "trace index counter overflowed");
+
+    // Get var args from start_tracing call.
+    auto Inputs = getTraceInputs(AOTMod->getFunction(FuncNames[0]), BBs[0]);
+
+    std::vector<Type *> InputTypes;
+    for (auto Val : Inputs) {
+      InputTypes.push_back(Val->getType());
+    }
+
+    // Create function to store compiled trace.
+    TraceName = string(TRACE_FUNC_PREFIX) + to_string(TraceIdx);
+    llvm::FunctionType *FType =
+        llvm::FunctionType::get(Type::getVoidTy(JITContext), InputTypes, false);
+    llvm::Function *JITFunc = llvm::Function::Create(
+        FType, Function::InternalLinkage, TraceName, JITMod);
+    JITFunc->setCallingConv(CallingConv::C);
+
+    // Create entry block and setup builder.
+    auto DstBB = BasicBlock::Create(JITContext, "", JITFunc);
+    llvm::IRBuilder<> Builder(JITContext);
+    Builder.SetInsertPoint(DstBB);
+
+    // Variables that are used (but not defined) inbetween start and stop
+    // tracing need to be replaced with function arguments which the user passes
+    // into the compiled trace. This loop creates a mapping from those original
+    // variables to the function arguments of the compiled trace function.
+    for (size_t Idx = 0; Idx != Inputs.size(); Idx++) {
+      Value *OldVal = Inputs[Idx];
+      Value *NewVal = JITFunc->getArg(Idx);
+      assert(NewVal->getType()->isPointerTy());
+      VMap[OldVal] = NewVal;
+    }
+
+    // A pointer to the call to YKTRACE_START in the AOT module (once
+    // encountered). When this changes from NULL to non-NULL, then we start
+    // copying instructions from the AOT module into the JIT module.
+    Instruction *StartTracingInstr = nullptr;
+
+    std::vector<CallInst *> inlined_calls;
+    CallInst *last_call = nullptr;
+    std::vector<GlobalVariable *> cloned_globals;
+
+    // Iterate over the trace and stitch together all traced blocks.
+    for (size_t Idx = 0; Idx < Len; Idx++) {
+      auto FuncName = FuncNames[Idx];
+
+      // FIXME: Deal with holes in the trace, e.g. calls to libc.
+      assert(FuncName != nullptr);
+
+      // Get a traced function so we can extract blocks from it.
+      Function *F = AOTMod->getFunction(FuncName);
+      if (!F)
+        errx(EXIT_FAILURE, "can't find function %s", FuncName);
+
+      // Skip to the correct block.
+      auto It = F->begin();
+      std::advance(It, BBs[Idx]);
+      BasicBlock *BB = &*It;
+
+      // Iterate over all instructions within this block and copy them over
+      // to our new module.
+      for (auto I = BB->begin(); I != BB->end(); I++) {
+        // Skip calls to debug intrinsics (e.g. @llvm.dbg.value). We don't
+        // currently handle debug info and these "pseudo-calls" cause our blocks
+        // to be prematurely terminated.
+        if (isa<DbgInfoIntrinsic>(I))
+          continue;
+
+        // If we've returned from a call, skip ahead to the instruction where
+        // we left off.
+        if (last_call != nullptr) {
+          if (&*I == last_call) {
+            last_call = nullptr;
+          }
+          continue;
+        }
+        if (isa<CallInst>(I)) {
+          CallInst *CI = cast<CallInst>(&*I);
+          Function *CF = CI->getCalledFunction();
+          if (CF == nullptr)
+            continue;
+
+          if (CF->getName() == YKTRACE_START) {
+            StartTracingInstr = &*I;
+            continue;
+          } else if (CF->getName() == YKTRACE_STOP) {
+            goto done;
+          } else {
+            // Skip remainder of this block and remember where we stopped so we
+            // can continue from this position after returning from the inlined
+            // call.
+            // FIXME Deal with calls we cannot or don't want to inline.
+            if (StartTracingInstr != nullptr) {
+              inlined_calls.push_back(CI);
+              // During inlining, remap function arguments to the variables
+              // passed in by the caller.
+              for (unsigned int i = 0; i < CI->arg_size(); i++) {
+                Value *Var = CI->getArgOperand(i);
+                Value *Arg = CF->getArg(i);
+                // If the operand has already been cloned into JITMod then we
+                // need to use the cloned value in the VMap.
+                if (VMap[Var] != nullptr)
+                  Var = VMap[Var];
+                VMap[Arg] = Var;
+              }
+              break;
+            }
+          }
+        }
+
+        // We don't start copying instructions into the JIT module until we've
+        // seen the call to YKTRACE_START.
+        if (StartTracingInstr == nullptr)
+          continue;
+
+        if (llvm::isa<llvm::BranchInst>(I)) {
+          // FIXME Replace all branch instruction with guards.
+          continue;
+        }
+
+        if (isa<ReturnInst>(I)) {
+          last_call = inlined_calls.back();
+          inlined_calls.pop_back();
+          // Replace the return variable of the call with its return value.
+          // Since the return value will have already been copied over to the
+          // JITModule, make sure we look up the copy.
+          auto OldRetVal = ((ReturnInst *)&*I)->getReturnValue();
+          if (isa<Constant>(OldRetVal)) {
+            VMap[last_call] = OldRetVal;
+          } else {
+            auto NewRetVal = VMap[OldRetVal];
+            VMap[last_call] = NewRetVal;
+          }
+          break;
+        }
+
+        // If execution reaches here, then the instruction I is to be copied
+        // into JITMod. Before we can do this, we have to scan the instruction's
+        // operands checking that each is defined in JITMod.
+        for (unsigned OpIdx = 0; OpIdx < I->getNumOperands(); OpIdx++) {
+          Value *Op = I->getOperand(OpIdx);
+          if (VMap[Op] == nullptr) {
+            // The operand is undefined in JITMod.
+            Type *OpTy = Op->getType();
+            if (isa<llvm::AllocaInst>(Op)) {
+              // In the AOT module, the operand is allocated on the stack with
+              // an `alloca`, but this variable is as-yet undefined in the JIT
+              // module.
+              //
+              // This happens because LLVM has a tendency to move allocas up to
+              // the first block of a function, and if we didn't trace that
+              // block (e.g. we started tracing in a later block), then we will
+              // have missed those allocations. In these cases we materialise
+              // the allocations as we see them used in code that *was* traced.
+              Value *Alloca =
+                  Builder.CreateAlloca(OpTy->getPointerElementType(),
+                                       OpTy->getPointerAddressSpace());
+              VMap[Op] = Alloca;
+            } else if (isa<GlobalVariable>(Op)) {
+              // If there's a reference to a GlobalVariable, copy it over to the
+              // new module.
+              GlobalVariable *OldGV = cast<GlobalVariable>(Op);
+              if (OldGV->isConstant()) {
+                // Global variable is a constant so just copy it into the trace.
+                // We don't need to check if this global already exists, since
+                // we're skipping any operand that's already been cloned into
+                // the VMap.
+                GlobalVariable *GV = new GlobalVariable(
+                    *JITMod, OldGV->getValueType(), OldGV->isConstant(),
+                    OldGV->getLinkage(), (Constant *)nullptr, OldGV->getName(),
+                    (GlobalVariable *)nullptr, OldGV->getThreadLocalMode(),
+                    OldGV->getType()->getAddressSpace());
+                GV->copyAttributesFrom(&*OldGV);
+                cloned_globals.push_back(OldGV);
+                VMap[OldGV] = GV;
+              } else {
+                // FIXME Allow trace to write to mutable global variables.
+                errx(EXIT_FAILURE, "Non-const global variable %s",
+                     OldGV->getName().data());
+              }
+            } else if (isa<Constant>(Op)) {
+              // The operand is a constant, so leave it as is.
+              VMap[Op] = Op;
+              continue;
+            } else if (Op == StartTracingInstr) {
+              // The value generated by StartTracingInstr is the thread tracer.
+              // At some optimisation levels, this gets stored in an alloca'd
+              // stack space. Since we've stripped the instruction that
+              // generates that value (from the JIT module), we have to make a
+              // dummy stack slot to keep LLVM happy.
+              Value *NullVal = Constant::getNullValue(OpTy);
+              VMap[Op] = NullVal;
+            }
+          }
+        }
+
+        // Shortly we will copy the instruction into the JIT module. We start by
+        // cloning the instruction.
+        auto NewInst = &*I->clone();
+
+        // FIXME: For now we strip debugging meta-data from the JIT module just
+        // so that the module will verify and compile. In the long run we should
+        // include the debug info for the trace code. This would entail copying
+        // over the various module-level debugging declarations that are
+        // dependencies of instructions with !dbg meta-data attached.
+        if (NewInst->hasMetadata()) {
+          SmallVector<std::pair<unsigned, MDNode *>> InstrMD;
+          NewInst->getAllMetadata(InstrMD);
+          for (auto &MD : InstrMD) {
+            if (MD.first != LLVMContext::MD_dbg)
+              continue;
+            NewInst->setMetadata(MD.first, NULL);
+          }
+        }
+
+        // Since the instruction operands still reference values from the AOT
+        // module, we must remap them to point to new values in the JIT module.
+        llvm::RemapInstruction(NewInst, VMap, RF_NoModuleLevelChanges);
+        VMap[&*I] = NewInst;
+
+#ifndef NDEBUG
+        RevVMap[NewInst] = &*I;
+#endif
+
+        // And finally insert the new instruction into the JIT module.
+        Builder.Insert(NewInst);
+      }
+    }
+
+    // If we fell out of the loop, then we never saw YKTRACE_STOP.
+    return NULL;
+
+  done:
+    Builder.CreateRetVoid();
+
+    // Fix initialisers/referrers for copied global variables.
+    // FIXME Do we also need to copy Linkage, MetaData, Comdat?
+    for (GlobalVariable *G : cloned_globals) {
+      GlobalVariable *NewGV = cast<GlobalVariable>(VMap[G]);
+      if (G->isDeclaration())
+        continue;
+
+      if (G->hasInitializer())
+        NewGV->setInitializer(MapValue(G->getInitializer(), VMap));
+    }
+    return JITMod;
+  }
+};
