8a9928354d9ce06b1232a74bf519785c7630bea8
Edd Barrett <vext01@gmail.com>
2021-08-20 10:17:16 +0100

    Fix indirectbr test for AOT opt levels > -O0.
    The indirect branch test uses computed gotos, as often seen in threaded
    dispatch in interpreters.
    
    The C code `goto *dispatch[argc];` (at higher AOT optimisation levels)
    is realised as IR like:
    
    ```
    @__const.main.dispatch = private unnamed_addr constant [3 x i8*] [i8* blockaddress(@main, %12), i8* blockaddress(@main, %13), i8* blockaddress(@main, %14)], align 16
    
    define dso_local i32 @main(i32 %0, i8** nocapture readnone %1) #0 !dbg !16 {
      ...
      %10 = getelementptr inbounds [3 x i8*], [3 x i8*]* @__const.main.dispatch, i64 0, i64 %9, !dbg !52
      %11 = load i8*, i8** %10, align 8, !dbg !52, !tbaa !53
      indirectbr i8* %11, [label %12, label %13, label %14]
      ...
    }
    ```
    
    Thus, there are several intermediate IR instructions and a global array
    involved in setting up the `indirectbr` instruction.
    
    When we create the trace code, It isn't necessary to copy the indirect
    branch into the `JITMod` as the successor block is known from the trace.
    However, naively not copying the branch would lead to dangling
    references in the IR because the `address` operand typically
    (indirectly) references AOT block addresses not present in the `JITMod`.
    Therefore we also remove the IR instruction which defines the `address`
    operand and anything which also becomes dead as a result (recursively).

--- c_tests/tests/indirect_branch.c
+++ c_tests/tests/indirect_branch.c
@@ -1,4 +1,3 @@
-// ignore: Referencing global in another module!
 // Compiler:
 // Run-time:
 //   env-var: YKD_PRINT_IR=aot
@@ -16,16 +15,22 @@
 #include <yk_testing.h>
 
 int main(int argc, char **argv) {
-  void *dispatch[] = {&&l1, &&l2};
+  // Note that LLVM knows that `l1` is dead code because `argc` is always >0.
+  void *dispatch[] = {&&l1, &&l2, &&l3};
   int z = 0;
 
   void *tt = __yktrace_start_tracing(HW_TRACING, &z, &argc);
   NOOPT_VAL(argc);
   goto *dispatch[argc];
 l1:
-  z = 10;
+  z = 7;
+  goto done;
 l2:
   z = 20;
+  goto done;
+l3:
+  z = 33;
+done:
   NOOPT_VAL(z);
   void *tr = __yktrace_stop_tracing(tt);
   assert(z == 20);
