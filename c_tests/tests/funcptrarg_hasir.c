df92bee5eba61535d00f80db73896afb74f0152c
Lukas Diekmann <lukas.diekmann@gmail.com>
2021-08-20 10:57:56 +0100

    Deal with indirect calls.
    This implements ways to deal with indirect function calls, i.e. calls
    where the callee isn't statically known, e.g.
    
    %1 = @foo
    call %1
    
    Since at the time we trace the call instruction we can't tell what %1
    is, we need to look ahead in the trace which function the tracer saw
    next.
    
    There's two scenarios we have to deal with: if we have IR for the passed
    function, we can inline it as usual (note that in the future we don't
    want to do this unless the function was promoted); if we don't have IR,
    we can't inline, so we leave the call as is (we do however need to make
    sure that the called function has been redeclared in the JITModule).

---
+++ c_tests/tests/funcptrarg_hasir.c
@@ -0,0 +1,35 @@
+// Compiler:
+// Run-time:
+
+// Test indirect calls where we have IR for the callee.
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yk_testing.h>
+
+__attribute__((noinline)) int foo(int a) { return a + 1; }
+
+int bar(int (*func)(int)) {
+  int a = func(3);
+  return a;
+}
+
+int main(int argc, char **argv) {
+  int z = 0;
+  void *tt = __yktrace_start_tracing(HW_TRACING, &z);
+  z = bar(foo);
+  NOOPT_VAL(z);
+  void *tr = __yktrace_stop_tracing(tt);
+  assert(z == 4);
+
+  void *ptr = __yktrace_irtrace_compile(tr);
+  __yktrace_drop_irtrace(tr);
+  void (*func)(void *) = (void (*)(void *))ptr;
+  int output = 0;
+  func(&output);
+  assert(output == 4);
+
+  return (EXIT_SUCCESS);
+}
