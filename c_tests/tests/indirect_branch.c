9f84a83f314620530b405aee6cb427fde46fc18a
Edd Barrett <vext01@gmail.com>
2021-11-22 12:03:02 +0000

    For now, crash upon encontering an `indirectbr`.
    We haven't yet implemented guards for `indirectbr` and currently no test
    encounters one (I've included a (skipped) test that we can use later).
    
    See https://github.com/ykjit/yk/issues/438 for details.

--- c_tests/tests/indirect_branch.c
+++ c_tests/tests/indirect_branch.c
@@ -1,52 +1,39 @@
-// ignore: broken during new control point design
+// ignore: guards for indirect branches not implemented.
 // Compiler:
+//   env-var: YKD_PRINT_JITSTATE=1
 // Run-time:
-//   env-var: YKD_PRINT_IR=aot
+//   env-var: YKD_PRINT_IR=jit-pre-opt
 //   stderr:
 //     ...
-//     indirectbr i8* %...
-//     ...
 
-// Check that we can handle indirect branches.
+// Check that tracing an `indirectbr` works.
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <yk.h>
 #include <yk_testing.h>
 
 int main(int argc, char **argv) {
-  // Note that LLVM knows that `l1` is dead code because `argc` is always >0.
-  void *dispatch[] = {&&l1, &&l2, &&l3};
-  int z = 0, idx = argc;
-
-  __yktrace_start_tracing(HW_TRACING, &z, &idx);
+  int loc = 0;
+  int i = 5;
+  int idx = 1;
+  void *dispatch[] = {&&label1, &&label3, &&label2};
+  NOOPT_VAL(i);
   NOOPT_VAL(idx);
-
-  // Now jump to l2 and then l3 via computed gotos.
-  goto *dispatch[idx];
-l1:
-  // unreachable.
-  exit(EXIT_FAILURE);
-l2:
-  z += 1;
-  idx += 1;
-  goto *dispatch[idx];
-l3:
-  z += 2;
-
-  NOOPT_VAL(z);
-  void *tr = __yktrace_stop_tracing();
-  printf("ZZZ %d\n", z);
-  assert(z == 3);
-
-  void *ptr = __yktrace_irtrace_compile(tr);
-  __yktrace_drop_irtrace(tr);
-  void (*func)(int *, int *) = (void (*)(int *, int *))ptr;
-  int z2 = 0;
-  int idx2 = argc;
-  func(&z2, &idx2);
-  assert(z2 == 3);
+  while (i > 0) {
+    yk_control_point(loc);
+    fprintf(stderr, "i=%d\n", i);
+    goto *dispatch[idx];
+label1:
+    abort(); // unreachable.
+label2:
+    abort(); // unreachable.
+label3:
+    i--;
+  }
+  abort(); // FIXME: unreachable due to aborting guard failure earlier.
 
   return (EXIT_SUCCESS);
 }
