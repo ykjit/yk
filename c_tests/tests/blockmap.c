27b9cf72b5e0e35c81353c153770152ca3081014
Edd Barrett <vext01@gmail.com>
2021-12-21 13:38:23 +0000

    Put back blockmap test.

--- c_tests/tests/blockmap.c
+++ c_tests/tests/blockmap.c
@@ -1,12 +1,11 @@
-// ignore: broken during new control point design
 // Compiler:
 // Run-time:
 
 // Check the blockmap for this test program contains blocks.
 
 #include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <yk.h>
 #include <yk_testing.h>
 
 int main(int argc, char **argv) {
@@ -14,4 +13,11 @@ int main(int argc, char **argv) {
   assert(__yktrace_hwt_mapper_blockmap_len(bm) > 0);
   __yktrace_hwt_mapper_blockmap_free(bm);
   return (EXIT_SUCCESS);
+}
+
+// This isn't used as part of the test, but is required for this file to
+// compile with ykllvm.
+void unused() {
+  YkLocation loc = yk_location_new();
+  yk_control_point(&loc);
 }
