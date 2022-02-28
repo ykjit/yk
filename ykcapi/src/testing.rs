36fd8fb1e0bf725496cff9acc58960aaddf10216
Laurence Tratt <laurie@tratt.net>
2022-02-28 20:57:52 +0000

    Break the testing code into its own module.

---
+++ ykcapi/src/testing.rs
@@ -0,0 +1,19 @@
+//! This module is only enabled when the `yk_testing` feature is enabled. It contains functions
+//! that are only needed when testing internal yk code.
+
+use yktrace::BlockMap;
+
+#[no_mangle]
+pub extern "C" fn __yktrace_hwt_mapper_blockmap_new() -> *mut BlockMap {
+    Box::into_raw(Box::new(BlockMap::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn __yktrace_hwt_mapper_blockmap_free(bm: *mut BlockMap) {
+    unsafe { Box::from_raw(bm) };
+}
+
+#[no_mangle]
+pub extern "C" fn __yktrace_hwt_mapper_blockmap_len(bm: *mut BlockMap) -> usize {
+    unsafe { &*bm }.len()
+}
