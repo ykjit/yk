b10398ce2b1ee03b613cf222dd3d4e15a98e9ca3
Edd Barrett <vext01@gmail.com>
2022-03-30 16:07:32 +0100

    Test the FastISel-related part of --yk-no-fallthrough.
    See https://github.com/ykjit/ykllvm/pull/30

---
+++ tests/tests/resume_and_branch.c
@@ -0,0 +1,46 @@
+// Run-time:
+//   env-var: YKD_PRINT_IR=aot
+//   env-var: YKD_SERIALISE_COMPILATION=1
+//   env-var: YKD_PRINT_JITSTATE=1
+//   stderr:
+//     ...
+//     jit-state: enter-jit-code
+//     x=2
+//     jit-state: exit-jit-code
+//     ...
+
+// Check that a call followed immediately by an unconditional branch doesn't
+// confuse the block mapping due to fallthrough optimisations.
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+__attribute__((noinline)) void f(int x) { fprintf(stderr, "x=%d\n", x); }
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new();
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  int x = 0;
+  int i = 4;
+  NOOPT_VAL(loc);
+  NOOPT_VAL(x);
+  NOOPT_VAL(i);
+  while (i > 0) {
+    yk_mt_control_point(mt, &loc);
+    f(i);
+    goto more;
+
+  more:
+    i--;
+  }
+
+  yk_location_drop(loc);
+  yk_mt_drop(mt);
+  return (EXIT_SUCCESS);
+}
