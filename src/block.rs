18ee8539e5c729edbb52f71d2ceb2e904826c9bb
Laurence Tratt <laurie@tratt.net>
2022-05-29 08:55:59 +0100

    Slightly abstract away from "the world is an x86".
    A type alias doesn't do much, but it least makes slightly clearer what
    `u64` means here. My guess is that in the medium term we should probably
    change this to something like `*const ()` or similar.

--- src/block.rs
+++ src/block.rs
@@ -1,15 +1,18 @@
+#[cfg(target_arch = "x86_64")]
+type BlockAddr = u64;
+
 /// Information about a basic block.
 #[derive(Debug, Eq, PartialEq)]
 pub struct Block {
     /// Virtual address of the first instruction in this block.
-    first_instr: u64,
+    first_instr: BlockAddr,
     /// Virtual address of the last instruction in this block.
-    last_instr: u64,
+    last_instr: BlockAddr,
 }
 
 impl Block {
     /// Creates a new basic block from a start address and a length in bytes.
-    pub fn new(first_instr: u64, last_instr: u64) -> Self {
+    pub fn new(first_instr: BlockAddr, last_instr: BlockAddr) -> Self {
         Self {
             first_instr,
             last_instr,
@@ -17,12 +20,12 @@ impl Block {
     }
 
     /// Returns the virtual address of the first instruction in this block.
-    pub fn first_instr(&self) -> u64 {
+    pub fn first_instr(&self) -> BlockAddr {
         self.first_instr
     }
 
     /// Returns the virtual address of the last instruction in this block.
-    pub fn last_instr(&self) -> u64 {
+    pub fn last_instr(&self) -> BlockAddr {
         self.last_instr
     }
 }
