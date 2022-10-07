6e797dd6ce3b087dc0fdec847d9ba8344d1c93bc
Edd Barrett <vext01@gmail.com>
2022-10-07 16:15:08 +0100

    Add a tool to help us gdb C tests.

---
+++ tests/src/bin/gdb_c_test.rs
@@ -0,0 +1,94 @@
+//! A tool to run a C test under gdb.
+
+use clap::Parser;
+use std::{env, path::PathBuf, process::Command};
+use tempfile::TempDir;
+use tests::{mk_compiler, EXTRA_LINK};
+
+/// Run a C test under gdb.
+#[derive(Parser, Debug)]
+#[command(about, long_about = None)]
+struct Args {
+    /// The test to attach gdb to.
+    test_file: PathBuf,
+
+    /// Run the test with `YKD_PRINT_IR` set to the specified value.
+    #[arg(short, long)]
+    print_ir: Option<String>,
+
+    /// Run the test with `YKD_PRINT_JITSTATE=1`
+    #[arg(short = 'j', long)]
+    print_jitstate: bool,
+
+    /// Run the test with `YKD_SERIALISE_COMPILATION=1`
+    #[arg(short, long)]
+    serialise_compilation: bool,
+
+    /// Set breakpoints at the first `N` compiled traces.
+    #[arg(short = 'b', long)]
+    num_breaks: Option<usize>,
+
+    /// Don't immediately run the program.
+    #[arg(short = 'n', long)]
+    wait_at_prompt: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let md = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let test_path = PathBuf::from(
+        [&md, "c", &args.test_file.to_str().unwrap()]
+            .iter()
+            .collect::<PathBuf>(),
+    );
+    let tempdir = TempDir::new().unwrap();
+
+    // Compile the test.
+    //
+    // Some tests expect to have extra objects linked.
+    let extra_objs = EXTRA_LINK
+        .get(&test_path.to_str().unwrap())
+        .unwrap_or(&Vec::new())
+        .iter()
+        .map(|e| e.generate_obj(tempdir.path()))
+        .collect::<Vec<PathBuf>>();
+
+    let binstem = PathBuf::from(args.test_file.file_stem().unwrap());
+    let binpath = [tempdir.path(), &binstem].iter().collect::<PathBuf>();
+    let mut cmd = mk_compiler(&binpath, &test_path, "-O0", &extra_objs);
+    if !cmd.spawn().unwrap().wait().unwrap().success() {
+        panic!("compilation failed");
+    }
+
+    // Now we have a test binary in a temporary directory, prepare an invocation of gdb, setting
+    // environment variables as necessary.
+    let mut gdb = Command::new("gdb");
+    gdb.arg(&binpath).env("YKD_TRACE_DEBUGINFO", "1");
+
+    if args.serialise_compilation {
+        gdb.env("YKD_SERIALISE_COMPILATION", "1");
+    }
+
+    if args.print_jitstate {
+        gdb.env("YKD_PRINT_JITSTATE", "1");
+    }
+
+    if let Some(irs) = args.print_ir {
+        gdb.env("YKD_PRINT_IR", irs);
+    }
+
+    if let Some(num_breaks) = args.num_breaks {
+        gdb.args(["-ex", "set breakpoint pending on"]); // don't prompt for pending breaks.
+        for i in 0..num_breaks {
+            gdb.args(["-ex", &format!("b __yk_compiled_trace_{i}")]);
+        }
+    }
+
+    if !args.wait_at_prompt {
+        gdb.args(["-ex", "run"]);
+    }
+
+    // Run gdb!
+    gdb.spawn().unwrap().wait().unwrap();
+}
---
+++ tests/src/lib.rs
@@ -0,0 +1,133 @@
+#![feature(once_cell)]
+
+use std::{
+    collections::HashMap,
+    env,
+    io::{self, Write},
+    path::{Path, PathBuf},
+    process::Command,
+    sync::LazyLock,
+};
+
+const TEMPDIR_SUBST: &'static str = "%%TEMPDIR%%";
+pub static EXTRA_LINK: LazyLock<HashMap<&'static str, Vec<ExtraLinkage>>> = LazyLock::new(|| {
+    let mut map = HashMap::new();
+
+    // These tests get an extra, separately compiled (thus opaque to LTO), object file linked in.
+    for test_file in &[
+        "call_ext_in_obj.c",
+        "loopy_funcs_not_inlined_by_default.c",
+        "not_loopy_funcs_inlined_by_default.c",
+        "unroll_safe_implies_noinline.c",
+        "unroll_safe_inlines.c",
+        "yk_unroll_safe_vs_yk_outline.c",
+    ] {
+        map.insert(
+            *test_file,
+            vec![ExtraLinkage::new(
+                "%%TEMPDIR%%/call_me.o",
+                &[
+                    "clang",
+                    "-c",
+                    "-O0",
+                    "extra_linkage/call_me.c",
+                    "-o",
+                    "%%TEMPDIR%%/call_me.o",
+                ],
+            )],
+        );
+    }
+    map
+});
+
+/// Describes an extra object file to link to a C test.
+pub struct ExtraLinkage<'a> {
+    /// The name of the object file to be generated.
+    output_file: &'a str,
+    /// The command that generates the object file.
+    gen_cmd: &'a [&'a str],
+}
+
+impl<'a> ExtraLinkage<'a> {
+    fn new(output_file: &'a str, gen_cmd: &'a [&'a str]) -> Self {
+        Self {
+            output_file,
+            gen_cmd,
+        }
+    }
+
+    /// Run the command to generate the object in `tempdir` and return the absolute path to the
+    /// generated object.
+    pub fn generate_obj(&self, tempdir: &Path) -> PathBuf {
+        let mut cmd = Command::new(self.gen_cmd[0]);
+        let tempdir_s = tempdir.to_str().unwrap();
+        for arg in self.gen_cmd[1..].iter() {
+            cmd.arg(arg.replace(TEMPDIR_SUBST, tempdir_s));
+        }
+        let out = cmd.output().unwrap();
+        assert!(tempdir.exists());
+        if !out.status.success() {
+            io::stdout().write_all(&out.stdout).unwrap();
+            io::stderr().write_all(&out.stderr).unwrap();
+            panic!();
+        }
+        let mut ret = PathBuf::from(tempdir);
+        ret.push(&self.output_file.replace(TEMPDIR_SUBST, tempdir_s));
+        ret
+    }
+}
+
+/// Make a compiler command that compiles `src` to `exe` using the optimisation flag `opt`.
+/// `extra_objs` is a collection of other object files to link.
+pub fn mk_compiler(exe: &Path, src: &Path, opt: &str, extra_objs: &[PathBuf]) -> Command {
+    let mut compiler = Command::new("clang");
+    compiler.env("YKD_PRINT_IR", "1");
+
+    let yk_config = [
+        &env::var("CARGO_MANIFEST_DIR").unwrap(),
+        "..",
+        "ykcapi",
+        "scripts",
+        "yk-config",
+    ]
+    .iter()
+    .collect::<PathBuf>();
+
+    #[cfg(cargo_profile = "debug")]
+    let mode = "debug";
+    #[cfg(cargo_profile = "release")]
+    let mode = "release";
+
+    let yk_config_out = Command::new(yk_config)
+        .args(&[mode, "--cflags", "--cppflags", "--ldflags", "--libs"])
+        .output()
+        .expect("failed to execute yk-config");
+    if !yk_config_out.status.success() {
+        panic!("yk-config exited with non-zero status");
+    }
+    let yk_flags = String::from_utf8(yk_config_out.stdout).unwrap();
+
+    // yk-config never returns arguments containing spaces, so we can split by space here. If this
+    // ever changes, then we should build arguments as an "unparsed" string and parse that to `sh
+    // -c` and let the shell do the parsing.
+    let yk_flags = yk_flags.trim().split(" ");
+    compiler.args(yk_flags);
+
+    compiler.args(&[
+        opt,
+        // If this is a debug build, include debug info in the test binary.
+        #[cfg(debug_assertions)]
+        "-g",
+        // Be strict.
+        "-Werror",
+        "-Wall",
+        // Some tests are multi-threaded via the pthread API.
+        "-pthread",
+        // The input and output files.
+        "-o",
+        exe.to_str().unwrap(),
+        src.to_str().unwrap(),
+    ]);
+    compiler.args(extra_objs);
+    compiler
+}
