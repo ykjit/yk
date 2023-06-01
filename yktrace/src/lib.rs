79652219abc060a176a94c0e5cc8a29ad4194c31
Laurence Tratt <laurie@tratt.net>
2023-06-01 10:25:53 +0100

    Remove unnecessary features.
    As code has shuffled around different crates, we've expanded the number
    of `#![feature(lazy_cell)]` lines, but not pruned them. This commit
    is a spring clean of the features which are no longer necessary.

--- yktrace/src/lib.rs
+++ yktrace/src/lib.rs
@@ -1,6 +1,5 @@
 //! Utilities for collecting and decoding traces.
 
-#![feature(lazy_cell)]
 #![feature(naked_functions)]
 #![allow(clippy::len_without_is_empty)]
 #![allow(clippy::new_without_default)]
