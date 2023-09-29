0e09683e10a924c568a32b30556ba05d902b0fd9
Edd Barrett <vext01@gmail.com>
2023-09-29 10:11:17 +0100

    Improve IR printing.
    We can now have a framework for printing variable names and things which
    require a table lookup in the AOT module.
    
    We now print unimplemented things uniformly using a `?type<what>`
    syntax.

--- ykrt/src/compile/jitc_yk/aot_ir.rs
+++ ykrt/src/compile/jitc_yk/aot_ir.rs
@@ -44,7 +44,7 @@ pub(crate) trait IRDisplay {
 
 /// An instruction opcode.
 #[deku_derive(DekuRead)]
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 #[deku(type = "u8")]
 pub(crate) enum Opcode {
     Nop = 0,
@@ -90,26 +90,33 @@ pub(crate) struct ConstantOperand {
 
 impl IRDisplay for ConstantOperand {
     fn to_str(&self, m: &AOTModule) -> String {
-        format!("{}", m.consts[self.constant_idx].to_str(m))
+        m.consts[self.constant_idx].to_str(m)
     }
 }
 
 #[deku_derive(DekuRead)]
 #[derive(Debug)]
 pub(crate) struct LocalVariableOperand {
+    #[deku(skip)] // computed after deserialisation.
+    func_idx: usize,
     bb_idx: usize,
     inst_idx: usize,
 }
 
 impl IRDisplay for LocalVariableOperand {
-    fn to_str(&self, _m: &AOTModule) -> String {
-        format!("${}_{}", self.bb_idx, self.inst_idx)
+    fn to_str(&self, m: &AOTModule) -> String {
+        format!(
+            "${}_{}: {}",
+            self.bb_idx,
+            self.inst_idx,
+            m.local_var_operand_type(self).to_str(m)
+        )
     }
 }
 
 const OPKIND_CONST: u8 = 0;
 const OPKIND_LOCAL_VARIABLE: u8 = 1;
-const OPKIND_STRING: u8 = 2;
+const OPKIND_UNIMPLEMENTED: u8 = 255;
 
 #[deku_derive(DekuRead)]
 #[derive(Debug)]
@@ -119,8 +126,8 @@ pub(crate) enum Operand {
     Constant(ConstantOperand),
     #[deku(id = "OPKIND_LOCAL_VARIABLE")]
     LocalVariable(LocalVariableOperand),
-    #[deku(id = "OPKIND_STRING")]
-    String(#[deku(until = "|v: &u8| *v == 0", map = "deserialise_string")] String),
+    #[deku(id = "OPKIND_UNIMPLEMENTED")]
+    Unimplemented(#[deku(until = "|v: &u8| *v == 0", map = "deserialise_string")] String),
 }
 
 impl IRDisplay for Operand {
@@ -128,7 +135,7 @@ impl IRDisplay for Operand {
         match self {
             Self::Constant(c) => c.to_str(m),
             Self::LocalVariable(l) => l.to_str(m),
-            Self::String(s) => format!("\"{}\"", s),
+            Self::Unimplemented(s) => format!("?op<{}>", s),
         }
     }
 }
@@ -137,6 +144,7 @@ impl IRDisplay for Operand {
 #[deku_derive(DekuRead)]
 #[derive(Debug)]
 pub(crate) struct Instruction {
+    type_index: usize,
     opcode: Opcode,
     #[deku(temp)]
     num_operands: u32,
@@ -149,6 +157,16 @@ pub(crate) struct Instruction {
 
 impl IRDisplay for Instruction {
     fn to_str(&self, m: &AOTModule) -> String {
+        if self.opcode == Opcode::Unimplemented {
+            debug_assert!(self.operands.len() == 1);
+            if let Operand::Unimplemented(s) = &self.operands[0] {
+                return format!("?inst<{}>", s);
+            } else {
+                // This would be an invalid serialisation.
+                panic!();
+            }
+        }
+
         if self.name.borrow().is_none() {
             m.compute_variable_names();
         }
@@ -157,11 +175,15 @@ impl IRDisplay for Instruction {
         if self.opcode.generates_value() {
             let name = self.name.borrow();
             // The unwrap cannot fail, as we forced computation of variable names above.
-            ret.push_str(&format!("${} = ", name.as_ref().unwrap()));
+            ret.push_str(&format!(
+                "${}: {} = ",
+                name.as_ref().unwrap(),
+                m.instr_type(self).to_str(m)
+            ));
         }
         ret.push_str(&self.opcode.to_str(m));
         if !self.operands.is_empty() {
-            ret.push_str(" ");
+            ret.push(' ');
         }
         let op_strs = self
             .operands
@@ -234,7 +256,6 @@ impl IRDisplay for Function {
 #[deku_derive(DekuRead)]
 #[derive(Debug, PartialEq, Eq)]
 pub(crate) struct IntegerType {
-    #[deku(temp)] // FIXME: untemp when needed.
     num_bits: u32,
 }
 
@@ -243,6 +264,12 @@ impl IntegerType {
         // FIXME: Implement printing of aribitrarily-sized (in bits) integers.
         // Consider using a bigint library so we don't have to do it ourself?
         String::from("SOMECONST")
+    }
+}
+
+impl IRDisplay for IntegerType {
+    fn to_str(&self, _m: &AOTModule) -> String {
+        format!("i{}", self.num_bits)
     }
 }
 
@@ -257,14 +284,23 @@ pub(crate) enum Type {
     #[deku(id = "TYKIND_INTEGER")]
     Integer(IntegerType),
     #[deku(id = "TYKIND_UNIMPLEMENTED")]
-    Unimplemented,
+    Unimplemented(#[deku(until = "|v: &u8| *v == 0", map = "deserialise_string")] String),
 }
 
 impl Type {
     fn const_to_str(&self, c: &Constant) -> String {
         match self {
             Self::Integer(it) => it.const_to_str(c),
-            Self::Unimplemented => String::from("SOMETYPE"),
+            Self::Unimplemented(s) => format!("?cst<{}>", s),
+        }
+    }
+}
+
+impl IRDisplay for Type {
+    fn to_str(&self, m: &AOTModule) -> String {
+        match self {
+            Self::Integer(i) => i.to_str(m),
+            Self::Unimplemented(s) => format!("?ty<{}>", s),
         }
     }
 }
@@ -326,6 +362,40 @@ impl AOTModule {
         }
     }
 
+    /// Fill in the function index of local variable operands of instructions.o
+    ///
+    /// FIXME: It may be possible to do this as we deserialise, instead of after the fact:
+    /// https://github.com/sharksforarms/deku/issues/363
+    fn compute_local_operand_func_indices(&mut self) {
+        for (f_idx, f) in self.funcs.iter_mut().enumerate() {
+            for bb in &mut f.blocks {
+                for inst in &mut bb.instrs {
+                    for op in &mut inst.operands {
+                        if let Operand::LocalVariable(ref mut lv) = op {
+                            lv.func_idx = f_idx;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// Get the type of the instruction.
+    ///
+    /// It is UB to pass an `instr` that is not from the `AOTModule` referenced by `self`.
+    fn instr_type(&self, instr: &Instruction) -> &Type {
+        &self.types[instr.type_index]
+    }
+
+    /// Get the type of the local variable operand.
+    ///
+    /// It is UB to pass an operand that is not from an instruction in the `AOTModule` referenced
+    /// by `self`.
+    fn local_var_operand_type(&self, o: &LocalVariableOperand) -> &Type {
+        let instr = &self.funcs[o.func_idx].blocks[o.bb_idx].instrs[o.inst_idx];
+        self.instr_type(instr)
+    }
+
     pub(crate) fn to_str(&self) -> String {
         let mut ret = String::new();
         ret.push_str(&format!("# IR format version: {}\n", self.version));
@@ -348,7 +418,10 @@ impl AOTModule {
 /// Deserialise an AOT module from the slice `data`.
 pub(crate) fn deserialise_module(data: &[u8]) -> Result<AOTModule, Box<dyn Error>> {
     match AOTModule::from_bytes((data, 0)) {
-        Ok(((_, _), modu)) => Ok(modu),
+        Ok(((_, _), mut modu)) => {
+            modu.compute_local_operand_func_indices();
+            Ok(modu)
+        }
         Err(e) => Err(e.to_string().into()),
     }
 }
@@ -357,7 +430,7 @@ pub(crate) fn deserialise_module(data: &[u8]) -> Result<AOTModule, Box<dyn Error
 mod tests {
     use super::{
         deserialise_module, deserialise_string, Opcode, FORMAT_VERSION, MAGIC, OPKIND_CONST,
-        TYKIND_UNIMPLEMENTED,
+        OPKIND_UNIMPLEMENTED, TYKIND_UNIMPLEMENTED,
     };
     use byteorder::{NativeEndian, WriteBytesExt};
     use std::ffi::CString;
@@ -390,6 +463,8 @@ mod tests {
         write_native_usize(&mut data, 2);
         // funcs[0].blocks[0].num_instrs
         write_native_usize(&mut data, 2);
+        // funcs[0].blocks[0].instrs[0].type_index
+        write_native_usize(&mut data, 0);
         // funcs[0].blocks[0].instrs[0].opcode
         data.write_u8(Opcode::Alloca as u8).unwrap();
         // funcs[0].blocks[0].instrs[0].num_operands
@@ -398,16 +473,23 @@ mod tests {
         data.write_u8(OPKIND_CONST).unwrap();
         // funcs[0].blocks[0].instrs[0].operands[0].const_idx
         write_native_usize(&mut data, 0);
+        // funcs[0].blocks[0].instrs[1].type_index
+        write_native_usize(&mut data, 0);
         // funcs[0].blocks[0].instrs[1].opcode
         data.write_u8(Opcode::Nop as u8).unwrap();
         // funcs[0].blocks[0].instrs[1].num_operands
         data.write_u32::<NativeEndian>(0).unwrap();
         // funcs[0].blocks[1].num_instrs
         write_native_usize(&mut data, 1);
+        // funcs[0].blocks[1].instrs[0].type_index
+        write_native_usize(&mut data, 0);
         // funcs[0].blocks[1].instrs[0].opcode
-        data.write_u8(Opcode::Nop as u8).unwrap();
+        data.write_u8(Opcode::Unimplemented as u8).unwrap();
         // funcs[0].blocks[1].instrs[0].num_operands
-        data.write_u32::<NativeEndian>(0).unwrap();
+        data.write_u32::<NativeEndian>(1).unwrap();
+        // funcs[0].blocks[1].instrs[0].operands[0].operand_kind
+        data.write_u8(OPKIND_UNIMPLEMENTED as u8).unwrap();
+        write_str(&mut data, "%3 = some_llvm_instruction ...");
 
         // funcs[1].name
         write_str(&mut data, "bar");
@@ -425,6 +507,7 @@ mod tests {
         write_native_usize(&mut data, 1);
         // types[0].type_kind
         data.write_u8(TYKIND_UNIMPLEMENTED).unwrap();
+        write_str(&mut data, "a_type");
 
         let test_mod = deserialise_module(data.as_slice()).unwrap();
         let string_mod = test_mod.to_str();
@@ -438,10 +521,10 @@ mod tests {
 
 func foo {
   bb0:
-    $0_0 = alloca SOMETYPE
+    $0_0: ?ty<a_type> = alloca ?cst<a_type>
     nop
   bb1:
-    nop
+    ?inst<%3 = some_llvm_instruction ...>
 }
 
 func bar;
