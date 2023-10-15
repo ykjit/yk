d85a718b5680780afc16d8962c2c7e09ff781ab9
Pavel Durov <pavel@p3ld3v.dev>
2023-10-15 13:23:22 +0100

    Add pthread_create interception.
    Intercepting pthread_create will allow thread-local ShadowStack
    allocation. Thread-local stack is essential to avoid race conditions
    where threads share the same global state.

---
+++ tests/c/pthread_create.c
@@ -0,0 +1,31 @@
+// Compiler:
+// Run-time:
+//   status: error
+//   stderr: ...
+//    not yet implemented: Allocate and set thread-local ShadowStack instance. No support for threads yet!
+//    ...
+
+#include <pthread.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+void *thread_function(void *arg) { return NULL; }
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new(NULL);
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  int i = 3;
+  while (i > 0) {
+    yk_mt_control_point(mt, &loc);
+    pthread_t thread;
+    pthread_create(&thread, NULL, thread_function, NULL);
+    pthread_join(thread, NULL);
+    i--;
+  }
+
+  yk_location_drop(loc);
+  yk_mt_drop(mt);
+  return 0;
+}
