216d5f01d965011ad8ca73020722adea2363b69b
Pavel Durov <pavel@p3ld3v.dev>
2023-10-16 17:41:17 +0100

    Add variables in pthread_create test.

--- tests/c/pthread_create.c
+++ tests/c/pthread_create.c
@@ -1,30 +1,42 @@
 // Compiler:
 // Run-time:
-//   status: error
-//   stderr: ...
-//    not yet implemented: Allocate and set thread-local ShadowStack instance. No support for threads yet!
-//    ...
+//   status: success
+//   stdout: 1,2,3,0
+//   stderr:
 
 #include <pthread.h>
+#include <stdio.h>
 #include <yk.h>
 #include <yk_testing.h>
 
-void *thread_function(void *arg) { return NULL; }
+void *thread_function(void *arg) {
+  int *a = (int *)arg;
+  printf("%d,", *a);
+  return NULL;
+}
 
 int main(int argc, char **argv) {
   YkMT *mt = yk_mt_new(NULL);
   yk_mt_hot_threshold_set(mt, 0);
   YkLocation loc = yk_location_new();
 
-  int i = 3;
-  while (i > 0) {
+  int MAX_THREADS = 3;
+  int thread_args[3] = {1, 2, 3};
+  pthread_t thread_handles[MAX_THREADS];
+
+  int a = 0;
+
+  for (int i = 0; i < MAX_THREADS; i++) {
     yk_mt_control_point(mt, &loc);
-    pthread_t thread;
-    pthread_create(&thread, NULL, thread_function, NULL);
-    pthread_join(thread, NULL);
-    i--;
+    pthread_create(&thread_handles[i], NULL, thread_function, &thread_args[i]);
+    pthread_join(thread_handles[i], NULL);
   }
 
+  if (a != 0) {
+    fprintf(stderr, "Expected 'a' variable to have value of 0\n");
+  } else {
+    printf("%d", a);
+  }
   yk_location_drop(loc);
   yk_mt_drop(mt);
   return 0;
