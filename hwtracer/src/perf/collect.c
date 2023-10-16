bedafd9259361840d17eb665547b91b4a993b166
Edd Barrett <vext01@gmail.com>
2023-10-16 12:10:40 +0100

    Re-use perf file descriptors for trace collection.
    Laurie and Lukas realised that opening Linux perf (`perf_event_open()`)
    can be very slow.
    
    To quote Laurie:
    
    > it's bimodal: one peak tightly clustered around 4ms, another peak
    > loosely clustered between 20-28ms
    
    This change therefore makes the JIT re-use perf file descriptors
    (per-thread).
    
    This required a small change to the state machine in the decoder.
    Previously we'd always expected a PSB+ sequence at the start of the
    trace, but with a re-used perf fd you won't get one until the next time
    `IA32_RTIT_STATUS.PacketByteCnt` exceeds the next emitted-bytes
    threshold.
    
    In general Intel recommend that you always scan for a PSB when starting
    to decode a trace. We get away without doing this because:
    
     a) For a new perf fd, the packet stream is always aligned at the start
        of a PSB packet.
    
     b) Nothing we do causes the packet stream to become desynchronised, so
        for a re-used perf fd, the packet stream is always packet-aligned.
    
    Co-authored-by: Laurence Tratt <laurie@tratt.net>

--- hwtracer/src/perf/collect.c
+++ hwtracer/src/perf/collect.c
@@ -20,6 +20,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <syscall.h>
+#include <threads.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -35,6 +36,17 @@
 #ifndef INFTIM
 #define INFTIM -1
 #endif
+
+/*
+ * The thread's perf file descriptor and its associated underlying `mmap(2)`
+ * regions. The file descriptor is re-used for subsequent trace collections for
+ * the same thread.
+ *
+ * FIXME: These leak when a thread dies.
+ */
+static thread_local void *cache_base_buf = NULL;
+static thread_local void *cache_aux_buf = NULL;
+static thread_local int cache_perf_fd = -1;
 
 enum hwt_cerror_kind {
   hwt_cerror_unused = 0,
@@ -518,7 +530,9 @@ hwt_perf_init_collector(struct hwt_perf_collector_config *tr_conf,
   tr_ctx->perf_fd = -1;
 
   // Obtain a file descriptor through which to speak to perf.
-  tr_ctx->perf_fd = open_perf(tr_conf->aux_bufsize, err);
+  if (cache_perf_fd == -1)
+    cache_perf_fd = open_perf(tr_conf->aux_bufsize, err);
+  tr_ctx->perf_fd = cache_perf_fd;
   if (tr_ctx->perf_fd == -1) {
     hwt_set_cerr(err, hwt_cerror_errno, errno);
     failing = true;
@@ -548,8 +562,10 @@ hwt_perf_init_collector(struct hwt_perf_collector_config *tr_conf,
   // data_bufsize'.
   int page_size = getpagesize();
   tr_ctx->base_bufsize = (1 + tr_conf->data_bufsize) * page_size;
-  tr_ctx->base_buf = mmap(NULL, tr_ctx->base_bufsize, PROT_WRITE, MAP_SHARED,
+  if (!cache_base_buf)
+    cache_base_buf = mmap(NULL, tr_ctx->base_bufsize, PROT_WRITE, MAP_SHARED,
                           tr_ctx->perf_fd, 0);
+  tr_ctx->base_buf = cache_base_buf;
   if (tr_ctx->base_buf == MAP_FAILED) {
     hwt_set_cerr(err, hwt_cerror_errno, errno);
     failing = true;
@@ -565,8 +581,10 @@ hwt_perf_init_collector(struct hwt_perf_collector_config *tr_conf,
   // Allocate the AUX buffer.
   //
   // Mapped R/W so as to have a saturating ring buffer.
-  tr_ctx->aux_buf = mmap(NULL, base_header->aux_size, PROT_READ | PROT_WRITE,
+  if (!cache_aux_buf)
+    cache_aux_buf = mmap(NULL, base_header->aux_size, PROT_READ | PROT_WRITE,
                          MAP_SHARED, tr_ctx->perf_fd, base_header->aux_offset);
+  tr_ctx->aux_buf = cache_aux_buf;
   if (tr_ctx->aux_buf == MAP_FAILED) {
     hwt_set_cerr(err, hwt_cerror_errno, errno);
     failing = true;
@@ -734,16 +752,6 @@ bool hwt_perf_free_collector(struct hwt_perf_ctx *tr_ctx,
                              struct hwt_cerror *err) {
   int ret = true;
 
-  if ((tr_ctx->aux_buf) &&
-      (munmap(tr_ctx->aux_buf, tr_ctx->aux_bufsize) == -1)) {
-    hwt_set_cerr(err, hwt_cerror_errno, errno);
-    ret = false;
-  }
-  if ((tr_ctx->base_buf) &&
-      (munmap(tr_ctx->base_buf, tr_ctx->base_bufsize) == -1)) {
-    hwt_set_cerr(err, hwt_cerror_errno, errno);
-    ret = false;
-  }
   if (tr_ctx->stop_fds[1] != -1) {
     // If the write end of the pipe is still open, the thread is still running.
     close(tr_ctx->stop_fds[1]); // signals thread to stop.
@@ -754,10 +762,6 @@ bool hwt_perf_free_collector(struct hwt_perf_ctx *tr_ctx,
   }
   if (tr_ctx->stop_fds[0] != -1) {
     close(tr_ctx->stop_fds[0]);
-  }
-  if (tr_ctx->perf_fd >= 0) {
-    close(tr_ctx->perf_fd);
-    tr_ctx->perf_fd = -1;
   }
   if (tr_ctx != NULL) {
     free(tr_ctx);
