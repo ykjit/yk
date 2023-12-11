f95a5f17cb2cfb2f993e5dbb877a9a74529dae78
Iti Shree <itishree1999@gmail.com>
2023-12-11 17:31:09 +0000

    ..

--- ykrt/pass_finder/cargo_run.py
+++ ykrt/pass_finder/cargo_run.py
@@ -5,14 +5,15 @@ def run_test(yk_path,env=None, n=1) :
         env = os.environ.copy()
     env['PATH'] = f"{yk_path}/bin:{env['PATH']}"
     env['YK_BUILD_TYPE'] = 'debug'
-    prelink_passes = (env or os.environ).get('PRELINK_PASSES', 'Not Set')
-    print(f"\033[93mPRELINK_PASSES: {prelink_passes}\033[0m")
+    postlink_passes = (env or os.environ).get('POSTLINK_PASSES', 'Not Set')
+    print(f"\033[93mPOSTLINK_PASSES: {postlink_passes}\033[0m")
     os.chdir(yk_path)
-    r = subprocess.run(["timeout 20 cargo test"], shell=True, env=env or os.environ)
+    r = subprocess.run(["timeout 50 cargo test"], shell=True, env=env or os.environ)
     # assert r.returncode == 0
     if r.returncode == 0:
         os.chdir("/home/shreei/research/yklua")
         times = []
+        c_test = None
         for _ in range(n):
             subprocess.run(["make clean"], shell=True, env=env)
             c = subprocess.run(["make && timeout 2 sh test.sh"], shell=True, env=env or os.environ)
@@ -22,10 +23,15 @@ def run_test(yk_path,env=None, n=1) :
                 c_test = subprocess.run(["timeout 2 sh run.sh"], shell=True, env=env or os.environ)
                 elapsed = time.time() - before
                 times.append(elapsed)
+
         # assert c_test.returncode == 0
-        mean_time = sum(times) / len(times)
+        assert len(times) == 0
+        if len(times) != 0:
+            mean_time = sum(times) / len(times)
+        else: 
+            mean_time = 0
         os.chdir(yk_path)
-        return c_test.returncode, mean_time
+        return c_test.returncode if (not c_test == None) else c.returncode, mean_time
     os.chdir(yk_path)
     return r.returncode, 0 
     # print(mean_time)
@@ -33,5 +39,4 @@ def run_test(yk_path,env=None, n=1) :
 
 code, time_p = run_test("/home/shreei/research/yk_pv")
 print(code, " ",  time_p)
-
 
