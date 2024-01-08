b179d7cd38f5c06aee45e10b01b907aac31ac5e3
Iti Shree <itishree1999@gmail.com>
2024-01-08 12:08:05 +0000

    cleaned cargo_run.py.

--- ykrt/pass_finder/cargo_run.py
+++ ykrt/pass_finder/cargo_run.py
@@ -3,47 +3,54 @@ import subprocess
 import time
 
 def run_test(yk_path, env=None, n=1):
-    if env is None:
-        env = os.environ.copy()
+    # Set path and environment variables
+    yk_path = os.environ.get('YK_PATH')
+    if yk_path is None:
+        raise ValueError("YK_PATH environment variable is not set")
+
+    yklua_path = os.environ.get('YKLUA_PATH')
+    if yklua_path is None:
+        raise ValueError("YKLUA_PATH environment variable is not set")
+
+    env = os.environ.copy()
     env['PATH'] = f"{yk_path}/bin:{env['PATH']}"
     env['YK_BUILD_TYPE'] = 'debug'
-    postlink_passes = (env or os.environ).get('POSTLINK_PASSES', 'Not Set')
-    print(f"\033[93mPOSTLINK_PASSES: {postlink_passes}\033[0m")
-    os.chdir(yk_path)
-    # assert r.returncode == 0
-    os.chdir("/home/shreei/research/yklua")
+     
+    # Check if pre and post-link flags are set
+    prelink_passes = env.get('PRELINK_PASSES', 'Not Set')
+    print(f"\n\033[93mPRELINK_PASSES: {prelink_passes}\033[0m")
+    postlink_passes = env.get('POSTLINK_PASSES', 'Not Set')
+    print(f"\n\033[93mPOSTLINK_PASSES: {postlink_passes}\033[0m")
+ 
+    curdir = os.getcwd()
+    os.chdir(yklua_path)
+
     times = []
-    c_test = None
-    env['IR_CHANGE'] = "false"
+    c_test = None 
+
     for _ in range(n):
-        # if IR changes returncode will be 0
-        c = subprocess.run(["timeout 40 sh test_ir.sh"], shell=True,
-                           env=env or os.environ)
+        c = subprocess.run(["timeout 30 sh test.sh"], shell=True, env=env)
         print(f"\033[95m returncode for c is {c.returncode} \033[0m\n")
-        # assert c.returncode == 0
+       
         os.chdir(yk_path)
-        r = subprocess.run(["timeout 50 cargo test"], shell=True,
-                           env=env or os.environ)
+        r = subprocess.run(["timeout 50 cargo test"], shell=True, env=env)
+
         if c.returncode == 0 and r.returncode == 0:
-            os.chdir("/home/shreei/research/yklua")
+            os.chdir(yklua_path)
             before = time.time()
-            c_test = subprocess.run(["timeout 15 sh run.sh"],
-                                    shell=True, env=env or os.environ)
+            c_test = subprocess.run(["timeout 8 sh run.sh"], shell=True, env=env)
             elapsed = time.time() - before
             times.append(elapsed)
 
-    #FIXME: return boolean variable IR CHANGE
-    # assert c_test.returncode == 0
     if len(times) != 0:
         mean_time = sum(times) / len(times)
     else:
         mean_time = 0
-        print(f"\033[95m mean time: {mean_time} \033[0m\n") 
-    os.chdir(yk_path)
+
     if c_test is None:
         return 130, 0
     
+    os.chdir(curdir)
     return c.returncode, mean_time
 
-code, time_p = run_test("/home/shreei/research/yk_pv")
-print(code, " ",  time_p)
+
