674fcd7a6016f210c83c1449cbc7e853dfd62765
Iti Shree <itishree1999@gmail.com>
2024-01-08 15:05:08 +0000

    fixed make command for yklua.

--- ykrt/pass_finder/cargo_run.py
+++ ykrt/pass_finder/cargo_run.py
@@ -31,14 +31,15 @@ def run_test(yk_path, env=None, n=1):
     c_test = None 
 
     for _ in range(n):
-        c = subprocess.run(["timeout 30 sh test.sh"], shell=True, env=env or os.environ)
+        subprocess.run(["make clean"], shell=True, env=env)
+        c = subprocess.run(["make && timeout 30 sh test.sh"], shell=True, env=env or os.environ)
         print(f"\033[95m returncode for c is {c.returncode} \033[0m\n")
        
         os.chdir(yk_path)
         r = subprocess.run(["timeout 50 cargo test"], shell=True, env=env or os.environ)
+        os.chdir(yklua_path) 
 
         if c.returncode == 0 and r.returncode == 0:
-            os.chdir(yklua_path)
             before = time.time()
             c_test = subprocess.run(["timeout 8 sh run.sh"], shell=True, env=env or os.environ)
             elapsed = time.time() - before
@@ -51,6 +52,8 @@ def run_test(yk_path, env=None, n=1):
 
     if c_test is None:
         return 130, 0
-    
+    elif c_test != 0:
+        return c_test, 0
+
     os.chdir(curdir)
-    return c.returncode, mean_time
+    return 0, mean_time
