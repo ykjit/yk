a94190f5d11a4e4263c2fb63eacec31c9aa603ca
Lukas Diekmann <lukas.diekmann@gmail.com>
2024-02-02 14:13:39 +0000

    Use TiVec.

--- ykrt/src/compile/jitc_yk/tracevec.rs
+++ ykrt/src/compile/jitc_yk/tracevec.rs
@@ -1,14 +1,17 @@
+use super::aot_ir::InstrIdx;
+use typed_index_collections::TiVec;
+
 use super::jit_ir::Instruction;
 
 /// A wrapper around a vector of instructions used to store a trace. Since we disallow removal and
 /// insertion (with the exception of appending at the end) into the vector, the wrapper only
 /// exposes the pushing and replacing of instructions.
 #[derive(Debug)]
-pub struct TraceVec(Vec<Instruction>);
+pub struct TraceVec(TiVec<InstrIdx, Instruction>);
 
 impl TraceVec {
     pub fn new() -> Self {
-        TraceVec(Vec::new())
+        TraceVec(Vec::new().into())
     }
 
     pub fn push(&mut self, instr: Instruction) {
