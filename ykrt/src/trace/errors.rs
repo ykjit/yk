b286a63df4dcfcd5f7720f9c549ca88331f84ccf
Laurence Tratt <laurie@tratt.net>
2024-02-22 17:47:52 +0000

    Communicate the failure of a too-long trace to be recorded to `MT`.

--- ykrt/src/trace/errors.rs
+++ ykrt/src/trace/errors.rs
@@ -7,6 +7,8 @@ use std::fmt::{self, Display, Formatter};
 pub enum InvalidTraceError {
     /// An empty trace was recorded.
     EmptyTrace,
+    /// The trace being recorded was too long and tracing was aborted.
+    TraceTooLong,
     /// Something went wrong in the compiler's tracing code.
     InternalError,
 }
@@ -15,6 +17,7 @@ impl Display for InvalidTraceError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
             InvalidTraceError::EmptyTrace => write!(f, "Empty trace"),
+            InvalidTraceError::TraceTooLong => write!(f, "Trace too long"),
             InvalidTraceError::InternalError => write!(f, "Internal tracing error"),
         }
     }
