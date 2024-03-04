8781d66b633444c2dc461cab46a0035882396f14
Laurence Tratt <laurie@tratt.net>
2024-03-04 17:13:39 +0000

    Make `map_blocks` write to a single vector.
    This stops us having to allocate lots of little vectors. Because trace
    mapping is currently so slow, there's not a meaningful speed-up from
    doing this, but it does slightly simplify the code, so it seems
    worthwhile purely on that basis.

--- ykrt/src/trace/errors.rs
+++ ykrt/src/trace/errors.rs
@@ -5,6 +5,8 @@ use std::fmt::{self, Display, Formatter};
 #[derive(Debug)]
 /// Reasons that a trace can be invalidated.
 pub enum InvalidTraceError {
+    /// Nothing was recorded.
+    TraceEmpty,
     /// The trace being recorded was too long and tracing was aborted.
     TraceTooLong,
 }
@@ -12,6 +14,7 @@ pub enum InvalidTraceError {
 impl Display for InvalidTraceError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
+            InvalidTraceError::TraceEmpty => write!(f, "Trace empty"),
             InvalidTraceError::TraceTooLong => write!(f, "Trace too long"),
         }
     }
--- ykrt/src/trace/hwt/mapper.rs
+++ ykrt/src/trace/hwt/mapper.rs
@@ -36,38 +36,7 @@ impl Iterator for HWTTraceIterator {
         while self.upcoming.len() < 2 {
             match self.hwt_iter.next() {
                 Some(Ok(x)) => {
-                    let mapped = map_block(&x);
-                    if mapped.is_empty() {
-                        // The block is unmappable. Note that we collapse consecutive unmappable
-                        // blocks.
-                        if self.upcoming.last() != Some(&TraceAction::new_unmappable_block()) {
-                            self.upcoming.push(TraceAction::new_unmappable_block());
-                            self.tas_generated += 1;
-                        }
-                    } else {
-                        for b in map_block(&x) {
-                            match b {
-                                Some(b) => {
-                                    if self.upcoming.last() != Some(&b) {
-                                        self.upcoming.push(b);
-                                        self.tas_generated += 1;
-                                    }
-                                }
-                                None => {
-                                    // Part of an hwtracer block mapped to a machine block in the
-                                    // LLVM block address map, but the machine block has no
-                                    // corresponding AOT LLVM IR blocks.
-                                    //
-                                    // FIXME: https://github.com/ykjit/yk/issues/388
-                                    // We *think* this happens because LLVM can introduce extra
-                                    // `MachineBasicBlock`s to help with laying out machine code.
-                                    // If that's the case, then for our purposes these extra blocks
-                                    // can be ignored. However, we should really investigate to be
-                                    // sure.
-                                }
-                            }
-                        }
-                    }
+                    self.map_block(&x);
                 }
                 Some(Err(HWTracerError::Unrecoverable(x)))
                     if x == "longjmp within traces currently unsupported" =>
@@ -102,123 +71,153 @@ impl Iterator for HWTTraceIterator {
 
 impl HWTTraceIterator {
     pub fn new(trace: Box<dyn Trace>) -> Result<Self, InvalidTraceError> {
-        let mut hwt_iter = trace.iter_blocks();
-
+        let mut hwti = HWTTraceIterator {
+            hwt_iter: trace.iter_blocks(),
+            upcoming: Vec::new(),
+            tas_generated: 0,
+        };
         // The first block contains the control point, which we need to remove.
         // As a rough proxy for "check that we removed only the thing we want to remove", we know
         // that the control point will be contained in a single mappable block. The `unwrap` can
         // only fail if our assumption about the block is incorrect (i.e. some part of the system
         // doesn't work as expected).
-        let first = hwt_iter.next().map(|x| map_block(&x.unwrap())).unwrap();
-        match first.as_slice() {
-            &[Some(TraceAction::MappedAOTBlock { .. })] => (),
-            _ => panic!(),
-        }
-        Ok(HWTTraceIterator {
-            hwt_iter,
-            upcoming: Vec::new(),
-            tas_generated: 0,
-        })
-    }
-}
-
-/// Maps one hwtracer block to one or more AOT LLVM IR blocks.
-///
-/// Mapping an hwtracer block to AOT LLVM IR blocks occurs in two phases. First the mapper
-/// tries to find machine blocks whose address ranges overlap with the address range of the
-/// hwtracer block (by using the LLVM block address map section). Once machine blocks have been
-/// found, the mapper then tries to find which LLVM IR blocks the machine blocks are part of.
-///
-/// A `Some` element in the returned vector means that the mapper found a machine block that
-/// maps to part of the hwtracer block and that the machine block could be directly mapped
-/// to an AOT LLVM IR block.
-///
-/// A `None` element in the returned vector means that the mapper found a machine block that
-/// corresponds with part of the hwtracer block but that the machine block could *not* be
-/// directly mapped to an AOT LLVM IR block. This happens when
-/// `MachineBasicBlock::getBasicBlock()` returns `nullptr`.
-///
-/// This function returns an empty vector if the hwtracer block was unmappable (no matching
-/// machine blocks could be found).
-///
-/// The reason we cannot simply ignore the `None` case is that it is important to differentiate
-/// "there were no matching machine blocks" from "there were matching machine blocks, but we
-/// were unable to find IR blocks for them".
-///
-/// The reason that there may be many corresponding blocks is due to the following scenario.
-///
-/// Suppose that the LLVM IR looked like this:
-///
-///   bb1:
-///     ...
-///     br bb2;
-///   bb2:
-///     ...
-///
-/// During codegen LLVM may remove the unconditional jump and simply place bb1 and bb2
-/// consecutively, allowing bb1 to fall-thru to bb2. In the eyes of hwtracer, a fall-thru does
-/// not terminate a block, so whereas LLVM sees two blocks, hwtracer sees only one.
-fn map_block(block: &hwtracer::Block) -> Vec<Option<TraceAction>> {
-    let b_rng = block.vaddr_range();
-    if b_rng.is_none() {
-        // If the address range of the block isn't known, then it follows that we can't map
-        // back to an IRBlock. We return the empty vector to flag this.
-        return Vec::new();
-    }
-    let (block_vaddr, block_last_instr) = b_rng.unwrap();
-
-    let (obj_name, block_off) = vaddr_to_obj_and_off(block_vaddr as usize).unwrap();
-
-    // Currently we only read in a block map and IR for the currently running binary (and not
-    // for dynamically linked shared objects). Thus, if we see code from another object, we
-    // can't map it.
-    //
-    // FIXME: https://github.com/ykjit/yk/issues/413
-    // In the future we could inline code from shared objects if they were built for use with
-    // yk (i.e. they have a blockmap and IR embedded).
-    if obj_name != *SELF_BIN_PATH {
-        return Vec::new();
-    }
-
-    let block_len = block_last_instr - block_vaddr;
-    let mut ret = Vec::new();
-    let mut ents = LLVM_BLOCK_MAP
-        .query(block_off, block_off + block_len)
-        .collect::<Vec<_>>();
-
-    // In the case that an hwtracer block maps to multiple machine blocks, it may be tempting
-    // to check that they are at consecutive address ranges. Unfortunately we can't do this
-    // because LLVM sometimes appends `nop` sleds (e.g. `nop word cs:[rax + rax]; nop`) to the
-    // ends of blocks for alignment. This padding is not reflected in the LLVM block address
-    // map, so blocks may not appear consecutive.
-    ents.sort_by(|x, y| x.range.start.partial_cmp(&y.range.start).unwrap());
-    for ent in ents {
-        if !ent.value.corr_bbs().is_empty() {
-            // OPT: This could probably be sped up with caching. If we use an interval tree
-            // keyed virtual address ranges, then we could take advantage of the fact that all
-            // blocks belonging to the same function will fall within the address range of the
-            // function's symbol. If the cache knows that block A and B are from the same
-            // function, and a block X has a start address between blocks A and B, then X must
-            // also belong to the same function and there's no need to query the linker.
-            // FIXME: Is this `unwrap` safe?
-            let sio = vaddr_to_sym_and_obj(usize::try_from(block_vaddr).unwrap()).unwrap();
-            debug_assert_eq!(
-                obj_name.to_str().unwrap(),
-                sio.dli_fname().unwrap().to_str().unwrap()
-            );
-            if let Some(sym_name) = sio.dli_sname() {
-                for bb in ent.value.corr_bbs() {
-                    ret.push(Some(TraceAction::new_mapped_aot_block(
-                        sym_name.to_owned(),
-                        usize::try_from(*bb).unwrap(),
-                    )));
+        match hwti.hwt_iter.next() {
+            Some(Ok(x)) => {
+                hwti.map_block(&x);
+                match hwti.upcoming.as_slice() {
+                    &[TraceAction::MappedAOTBlock { .. }] => {
+                        hwti.upcoming.pop();
+                        Ok(hwti)
+                    }
+                    _ => panic!(),
                 }
-            } else {
-                ret.push(None);
             }
-        } else {
-            ret.push(None);
+            Some(Err(_)) => todo!(),
+            None => Err(InvalidTraceError::TraceEmpty),
         }
     }
-    ret
+
+    /// Maps one hwtracer block to one or more AOT LLVM IR blocks.
+    ///
+    /// Mapping an hwtracer block to AOT LLVM IR blocks occurs in two phases. First the mapper
+    /// tries to find machine blocks whose address ranges overlap with the address range of the
+    /// hwtracer block (by using the LLVM block address map section). Once machine blocks have been
+    /// found, the mapper then tries to find which LLVM IR blocks the machine blocks are part of.
+    ///
+    /// A `Some` element in the returned vector means that the mapper found a machine block that
+    /// maps to part of the hwtracer block and that the machine block could be directly mapped
+    /// to an AOT LLVM IR block.
+    ///
+    /// A `None` element in the returned vector means that the mapper found a machine block that
+    /// corresponds with part of the hwtracer block but that the machine block could *not* be
+    /// directly mapped to an AOT LLVM IR block. This happens when
+    /// `MachineBasicBlock::getBasicBlock()` returns `nullptr`.
+    ///
+    /// This function returns an empty vector if the hwtracer block was unmappable (no matching
+    /// machine blocks could be found).
+    ///
+    /// The reason we cannot simply ignore the `None` case is that it is important to differentiate
+    /// "there were no matching machine blocks" from "there were matching machine blocks, but we
+    /// were unable to find IR blocks for them".
+    ///
+    /// The reason that there may be many corresponding blocks is due to the following scenario.
+    ///
+    /// Suppose that the LLVM IR looked like this:
+    ///
+    ///   bb1:
+    ///     ...
+    ///     br bb2;
+    ///   bb2:
+    ///     ...
+    ///
+    /// During codegen LLVM may remove the unconditional jump and simply place bb1 and bb2
+    /// consecutively, allowing bb1 to fall-thru to bb2. In the eyes of hwtracer, a fall-thru does
+    /// not terminate a block, so whereas LLVM sees two blocks, hwtracer sees only one.
+    fn map_block(&mut self, block: &hwtracer::Block) {
+        let b_rng = block.vaddr_range();
+        if b_rng.is_none() {
+            // If the address range of the block isn't known, then it follows that we can't map
+            // back to an IRBlock. We return the empty vector to flag this.
+            self.push_upcoming(TraceAction::new_unmappable_block());
+            return;
+        }
+        let (block_vaddr, block_last_instr) = b_rng.unwrap();
+
+        let (obj_name, block_off) = vaddr_to_obj_and_off(block_vaddr as usize).unwrap();
+
+        // Currently we only read in a block map and IR for the currently running binary (and not
+        // for dynamically linked shared objects). Thus, if we see code from another object, we
+        // can't map it.
+        //
+        // FIXME: https://github.com/ykjit/yk/issues/413
+        // In the future we could inline code from shared objects if they were built for use with
+        // yk (i.e. they have a blockmap and IR embedded).
+        if obj_name != *SELF_BIN_PATH {
+            self.push_upcoming(TraceAction::new_unmappable_block());
+            return;
+        }
+
+        let block_len = block_last_instr - block_vaddr;
+        let mut ents = LLVM_BLOCK_MAP
+            .query(block_off, block_off + block_len)
+            .collect::<Vec<_>>();
+
+        // In the case that an hwtracer block maps to multiple machine blocks, it may be tempting
+        // to check that they are at consecutive address ranges. Unfortunately we can't do this
+        // because LLVM sometimes appends `nop` sleds (e.g. `nop word cs:[rax + rax]; nop`) to the
+        // ends of blocks for alignment. This padding is not reflected in the LLVM block address
+        // map, so blocks may not appear consecutive.
+        ents.sort_by(|x, y| x.range.start.partial_cmp(&y.range.start).unwrap());
+        for ent in ents {
+            if !ent.value.corr_bbs().is_empty() {
+                // OPT: This could probably be sped up with caching. If we use an interval tree
+                // keyed virtual address ranges, then we could take advantage of the fact that all
+                // blocks belonging to the same function will fall within the address range of the
+                // function's symbol. If the cache knows that block A and B are from the same
+                // function, and a block X has a start address between blocks A and B, then X must
+                // also belong to the same function and there's no need to query the linker.
+                // FIXME: Is this `unwrap` safe?
+                let sio = vaddr_to_sym_and_obj(usize::try_from(block_vaddr).unwrap()).unwrap();
+                debug_assert_eq!(
+                    obj_name.to_str().unwrap(),
+                    sio.dli_fname().unwrap().to_str().unwrap()
+                );
+                if let Some(sym_name) = sio.dli_sname() {
+                    for bb in ent.value.corr_bbs() {
+                        self.push_upcoming(TraceAction::new_mapped_aot_block(
+                            sym_name.to_owned(),
+                            usize::try_from(*bb).unwrap(),
+                        ));
+                    }
+                    continue;
+                }
+            }
+            // If we got here, it is because part of an hwtracer block mapped to a machine block in the
+            // LLVM block address map, but the machine block has no corresponding AOT LLVM IR blocks.
+            //
+            // FIXME: https://github.com/ykjit/yk/issues/388 We *think* this happens because LLVM can
+            // introduce extra `MachineBasicBlock`s to help with laying out machine code. If that's the
+            // case, then for our purposes these extra blocks can be ignored. However, we should really
+            // investigate to be sure.
+        }
+    }
+
+    /// Push `new` into `self.upcoming` *unless* `new` is equal to `self.upcoming.last`, at which
+    /// point `new` will be ignored. In other words, this function can be thought of as "push and
+    /// deduplicate".
+    ///
+    /// Note that duplication can happen for two reasons:
+    ///
+    ///   1. Repeated unmappable blocks.
+    ///   2. Repeated mappable blocks. The `BlockDisambiguate` pass in ykllvm ensures that no
+    ///      high-level LLVM IR block ever branches straight back to itself, so if we see the same
+    ///      high-level block more than once consecutively in a trace, then we know that the IR
+    ///      block has been lowered to multiple machine blocks during code-gen, and that we should
+    ///      only push the IR block once.
+    fn push_upcoming(&mut self, new: TraceAction) {
+        if self.upcoming.last() != Some(&new) {
+            self.upcoming.push(new);
+            self.tas_generated += 1;
+        }
+    }
 }
