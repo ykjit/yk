d52a8018098207d86945596ab3c00e45e45e36f1
Pavel Durov <pavel@p3ld3v.dev>
2024-03-14 11:38:59 +0000

    Add tests for goto and indirect external calls.

---
+++ tests/c/goto_loop.c
@@ -0,0 +1,28 @@
+// Run-time:
+//   stdout: 1000
+//   stderr:
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new(NULL);
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  int i = 0;
+  NOOPT_VAL(i);
+loop:
+  yk_mt_control_point(mt, &loc);
+  if (i < 1000) {
+    i++;
+    goto loop;
+  }
+  printf("%d", i);
+  yk_location_drop(loc);
+  yk_mt_drop(mt);
+  return (EXIT_SUCCESS);
+}
