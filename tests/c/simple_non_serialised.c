e0cfdbadf6b0644062525467f28630947975d562
Laurence Tratt <laurie@tratt.net>
2024-03-19 08:40:29 +0000

    Allow event-based tests.
    Because yk tries to do as much work as possible in threads, many of our
    tests have *had* to use `YKD_SERIALISED_COMPILATION` in order that they
    can guarantee that they're testing (e.g.) compiled code.
    
    This commit adds a crude, but effective, ability for a test thread to
    suspend itself (without consuming undue CPU!) until a set of yk events
    have happened.
    
    The `simple_non_serialised` test is an example of this:
    
    ```c
    while (i > 0) {
      yk_mt_control_point(mt, &loc);
      if (i == 3)
        __ykstats_wait_until(mt, test_compiled_event);
      ...
    }
    
    bool test_compiled_event(YkCStats stats) {
      return stats.traces_compiled_ok == 1;
    }
    ```
    
    In essence, `__ykstats_wait_until` runs the `test_compiled_event`
    whenever a yk event such as "a trace has been compiled" has occurred. If
    `test_compiled_event` returns false, the thread will be parked until
    another event is occurred. If and when `test_compiled_event` returns
    true, `__ykstats_wait_until` itself returns.
    
    At the moment the "events" one can wait on are crude: you can detect,
    for example, the number of traces that have been compiled, but not that
    a *particular* location has been compiled. Will this turn out to be
    flexible enough in the long run? Time will tell. In the interim, this
    relatively simple mechanism should allow us to write tests that more
    accurately model yk running in the real-world.

---
+++ tests/c/simple_non_serialised.c
@@ -0,0 +1,54 @@
+// Run-time:
+//   env-var: YKD_PRINT_JITSTATE=1
+//   env-var: YKD_STATS=1
+//   stderr:
+//     jit-state: start-tracing
+//     i=4
+//     jit-state: stop-tracing
+//     i=3
+//     jit-state: enter-jit-code
+//     i=2
+//     i=1
+//     jit-state: deoptimise
+//   stdout:
+//     exit
+
+// Check that basic trace compilation in a thread works.
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+bool test_compiled_event(YkCStats);
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new(NULL);
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  int res = 9998;
+  int i = 4;
+  NOOPT_VAL(loc);
+  NOOPT_VAL(res);
+  NOOPT_VAL(i);
+  while (i > 0) {
+    yk_mt_control_point(mt, &loc);
+    if (i == 3)
+      __ykstats_wait_until(mt, test_compiled_event);
+    fprintf(stderr, "i=%d\n", i);
+    res += 2;
+    i--;
+  }
+  printf("exit");
+  NOOPT_VAL(res);
+  yk_location_drop(loc);
+  yk_mt_drop(mt);
+  return (EXIT_SUCCESS);
+}
+
+bool test_compiled_event(YkCStats stats) {
+  return stats.traces_compiled_ok == 1;
+}
