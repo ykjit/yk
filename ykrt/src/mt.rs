ykjit/yk

commit ff72e72f40ad894935d8cc73a40ff005e6cf671a

When `yk_testing` is enabled, make grabbing a Location lock deterministic.
In normal use, when we try and grab a `Location`s lock, we give up
entirely if it looks like it will take too long to grab the lock. This
solves a thundering herd problem that at least one of mt's unit tests
highlights.

However, it means that it is then non-deterministic when we encounter a
`Compiled` `Location` as to whether we fall back to the interpreter or
not. All of our C tests could technically have fallen pray to this, but
because we set `YKD_SERIALISED=1` and only used a single thread we
(mostly?) got away with it on x86. However the new event based framework
makes it much more likely that we notice this problem, because now we
genuinely are using multiple threads

This commit uses the `yk_testing` feature as a proxy for "make grabbing
a `Location`s lock deterministic".

ykrt/src/mt.rs
@@ -18,6 +18,7 @@ use std::{
 };
 
 use parking_lot::{Condvar, Mutex, MutexGuard};
+#[cfg(not(feature = "yk_jitstate_debug"))]
 use parking_lot_core::SpinWait;
 #[cfg(feature = "yk_jitstate_debug")]
 use std::sync::LazyLock;
@@ -347,34 +348,49 @@ impl MT {
             match loc.hot_location() {
                 Some(hl) => {
                     // If this thread is tracing something, we *must* grab the [HotLocation] lock,
-                    // because we need to know for sure if `loc` is the point at which we should stop
-                    // tracing. If this thread is not tracing anything, however, it's not worth
-                    // contending too much with other threads: we try moderately hard to grab the lock,
-                    // but we don't want to park this thread.
-                    let mut lk = if !am_tracing {
-                        // This thread isn't tracing anything, so we try for a little while to grab the
-                        // lock, before giving up and falling back to the interpreter. In general, we
-                        // expect that we'll grab the lock rather quickly. However, there is one nasty
-                        // use-case, which is when an army of threads all start executing the same
-                        // piece of tiny code and end up thrashing away at a single Location,
-                        // particularly when it's in a non-Compiled state: we can end up contending
-                        // horribly for a single lock, and not making much progress. In that case, it's
-                        // probably better to let some threads fall back to the interpreter for another
-                        // iteration, and hopefully allow them to get sufficiently out-of-sync that
-                        // they no longer contend on this one lock as much.
-                        let mut sw = SpinWait::new();
-                        loop {
-                            if let Some(lk) = hl.try_lock() {
-                                break lk;
+                    // because we need to know for sure if `loc` is the point at which we should
+                    // stop tracing. In most compilation modes, we are willing to give up trying to
+                    // lock and return if it looks like it will take too long. When `yk_testing` is
+                    // enabled, however, this introduces non-determinism, so in that compilation
+                    // mode only we guarantee to grab the lock.
+                    let mut lk;
+
+                    #[cfg(not(feature = "yk_testing"))]
+                    {
+                        // If this thread is not tracing anything, however, it's not worth
+                        // contending too much with other threads: we try moderately hard to grab
+                        // the lock, but we don't want to park this thread.
+                        if !am_tracing {
+                            // This thread isn't tracing anything, so we try for a little while to grab the
+                            // lock, before giving up and falling back to the interpreter. In general, we
+                            // expect that we'll grab the lock rather quickly. However, there is one nasty
+                            // use-case, which is when an army of threads all start executing the same
+                            // piece of tiny code and end up thrashing away at a single Location,
+                            // particularly when it's in a non-Compiled state: we can end up contending
+                            // horribly for a single lock, and not making much progress. In that case, it's
+                            // probably better to let some threads fall back to the interpreter for another
+                            // iteration, and hopefully allow them to get sufficiently out-of-sync that
+                            // they no longer contend on this one lock as much.
+                            let mut sw = SpinWait::new();
+                            loop {
+                                if let Some(x) = hl.try_lock() {
+                                    lk = x;
+                                    break;
+                                }
+                                if !sw.spin() {
+                                    return TransitionControlPoint::NoAction;
+                                }
                             }
-                            if !sw.spin() {
-                                return TransitionControlPoint::NoAction;
-                            }
-                        }
-                    } else {
-                        // This thread is tracing something, so we must grab the lock.
-                        hl.lock()
-                    };
+                        } else {
+                            // This thread is tracing something, so we must grab the lock.
+                            lk = hl.lock();
+                        };
+                    }
+
+                    #[cfg(feature = "yk_testing")]
+                    {
+                        lk = hl.lock();
+                    }
 
                     match lk.kind {
                         HotLocationKind::Compiled(ref ctr) => {

commit 0ec9e987e877e6c983ce25bf59e28164af65e930

Only use "always lock" when `yk_testing` is set.
`mt.rs`'s unit tests need to test the "real-world sometimes-can-fail"
locking, but ff72e72f accidentally caused those unit tests with the same
"always lock" code as when `yk_testing` is set. This commit fixes that.

Happily this commit also restores the speed of mt's unit tests. In
particular, the `only_one_thread_starts_tracing` test is hugely slow
when `yk_testing` is enabled, taking up to 80-90s to run on my machine,
instead of the 2-3 when `yk_testing` is disabled.

ykrt/src/mt.rs
@@ -18,7 +18,7 @@ use std::{
 };
 
 use parking_lot::{Condvar, Mutex, MutexGuard};
-#[cfg(not(feature = "yk_jitstate_debug"))]
+#[cfg(not(all(feature = "yk_testing", not(test))))]
 use parking_lot_core::SpinWait;
 #[cfg(feature = "yk_jitstate_debug")]
 use std::sync::LazyLock;
@@ -355,7 +355,7 @@ impl MT {
                     // mode only we guarantee to grab the lock.
                     let mut lk;
 
-                    #[cfg(not(feature = "yk_testing"))]
+                    #[cfg(not(all(feature = "yk_testing", not(test))))]
                     {
                         // If this thread is not tracing anything, however, it's not worth
                         // contending too much with other threads: we try moderately hard to grab
@@ -387,7 +387,7 @@ impl MT {
                         };
                     }
 
-                    #[cfg(feature = "yk_testing")]
+                    #[cfg(all(feature = "yk_testing", not(test)))]
                     {
                         lk = hl.lock();
                     }