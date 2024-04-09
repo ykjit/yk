44011539f55d86055d5f1a9d69452069d90397d4
Pavel Durov <pavel@p3ld3v.dev>
2024-04-09 16:35:15 +0100

    Format.

--- ykrt/src/trace/swt/patch.rs
+++ ykrt/src/trace/swt/patch.rs
@@ -72,11 +72,7 @@ unsafe fn patch_function(function_ptr: usize, code: *const u8, size: size_t) {
         - (func_address as usize)) as usize;
 
     // Set function memory region to be writable
-    let result = mprotect(
-        func_address,
-        page_size_aligned,
-        PROT_READ | PROT_WRITE,
-    );
+    let result = mprotect(func_address, page_size_aligned, PROT_READ | PROT_WRITE);
     if result != 0 {
         panic!("Failed to change memory protection to be writable");
     }
