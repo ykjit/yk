e46723a7df248bc076d991f47b806b24f067b004
Laurence Tratt <laurie@tratt.net>
2024-05-03 14:12:43 +0100

    Add `--check` to `xtask cfmt`.
    After my confusion yesterday at how cfmt worked in `.buildbot.sh`, I
    realised that it would be easiest to just mirror how rustfmt works.
    
    This commit adds a `--check` flag to cfmt: if it detects unformatted C
    files, it will exit. Without the `--check` flag, this commit doesn't
    change anything (i.e. by default cfmt modifies files).
    
    This should have (or at least get us closer to having) an additional
    advantage: at the moment `tryci` only works if all the code is
    committed, because `git diff --exit-code` caused an error, even if no C
    formatting had occurred. This commit allows `tryci` to work even with
    uncommitted changes.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::process::Command;
+use std::process::{exit, Command};
 use walkdir::{DirEntry, WalkDir};
 use ykbuild::ykllvm_bin;
 
@@ -14,7 +14,8 @@ fn ignore_dir(entry: &DirEntry) -> bool {
     true
 }
 
-fn clang_format() {
+fn clang_format(check_only: bool) {
+    let mut failed = false;
     for entry in WalkDir::new(".")
         .into_iter()
         .filter_entry(ignore_dir)
@@ -28,47 +29,85 @@ fn clang_format() {
                 "h" | "c" | "cpp" | "cc" => {
                     let clang_format = ykllvm_bin("clang-format");
                     let mut err_msg = None;
-                    match Command::new(&clang_format)
-                        .arg("-i")
-                        .arg(entry.path())
-                        .output()
-                    {
-                        Ok(r) => {
-                            let stderr =
-                                std::str::from_utf8(&r.stderr).unwrap_or("<non UTF-8 stderr>");
-                            match r.status.code() {
-                                Some(0) => (),
-                                Some(c) => {
-                                    err_msg = Some(format!("returned exit code {c}:\n\n{stderr}"))
-                                }
-                                None => {
-                                    err_msg = Some("terminated by signal:\n\n{stderr}".to_owned())
+                    if check_only {
+                        match Command::new(&clang_format)
+                            .args(["--dry-run", "-ferror-limit=1", "-Werror"])
+                            .arg(entry.path())
+                            .output()
+                        {
+                            Ok(r) => {
+                                if !r.status.success() {
+                                    eprintln!(
+                                        "{}",
+                                        std::str::from_utf8(&r.stderr)
+                                            .unwrap_or("<non UTF-8 stderr>")
+                                    );
+                                    failed = true;
                                 }
                             }
+                            Err(e) => panic!("{e:?}"),
                         }
-                        Err(e) => err_msg = Some(e.to_string()),
-                    }
-                    if let Some(m) = err_msg {
-                        panic!(
-                            "{} -i {}: {m}",
-                            clang_format.as_path().to_str().unwrap(),
-                            entry.path().to_str().unwrap()
-                        );
+                    } else {
+                        match Command::new(&clang_format)
+                            .arg("-i")
+                            .arg(entry.path())
+                            .output()
+                        {
+                            Ok(r) => {
+                                let stderr =
+                                    std::str::from_utf8(&r.stderr).unwrap_or("<non UTF-8 stderr>");
+                                match r.status.code() {
+                                    Some(0) => (),
+                                    Some(c) => {
+                                        err_msg =
+                                            Some(format!("returned exit code {c}:\n\n{stderr}"))
+                                    }
+                                    None => {
+                                        err_msg =
+                                            Some("terminated by signal:\n\n{stderr}".to_owned())
+                                    }
+                                }
+                            }
+                            Err(e) => err_msg = Some(e.to_string()),
+                        }
+                        if let Some(m) = err_msg {
+                            panic!(
+                                "{} -i {}: {m}",
+                                clang_format.as_path().to_str().unwrap(),
+                                entry.path().to_str().unwrap()
+                            );
+                        }
                     }
                 }
                 _ => {}
             }
         }
     }
+
+    if failed {
+        exit(1);
+    }
+}
+
+fn usage() -> ! {
+    eprintln!(
+        "Please specify task to run:
+cfmt [--check]    Formats C/C++ files with clang-format."
+    );
+    exit(1)
 }
 
 fn main() {
-    let task = env::args().nth(1);
-    match task.as_deref() {
-        Some("cfmt") => clang_format(),
-        _ => println!(
-            "Please specify task to run:
-cfmt       Formats C/C++ files with clang-format."
-        ),
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    match args.first().map(|x| x.as_str()) {
+        Some("cfmt") => {
+            let check_only = match args.get(1).map(|x| x.as_str()) {
+                Some("--check") => true,
+                Some(_) => usage(),
+                None => false,
+            };
+            clang_format(check_only);
+        }
+        _ => usage(),
     }
 }
