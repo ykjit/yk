57a9129590627652362ad282960d3f1702ca7208
Edd Barrett <vext01@gmail.com>
2024-05-13 15:08:34 +0100

    Add tests that use negative PtrAdd offsets.

---
+++ tests/c/neg_ptradd.newcg.c
@@ -0,0 +1,58 @@
+// ignore-if: test $YK_JIT_COMPILER != "yk" -o "$YKB_TRACER" = "swt"
+// Run-time:
+//   env-var: YKD_LOG_IR=-:aot,jit-pre-opt
+//   env-var: YKD_SERIALISE_COMPILATION=1
+//   env-var: YKD_LOG_JITSTATE=-
+//   stderr:
+//     jitstate: start-tracing
+//     i=9
+//     jitstate: stop-tracing
+//     --- Begin aot ---
+//     ...
+//     ${{14_2}}: ptr = PtrAdd ${{14_1}}, -{{4}}
+//     ...
+//     --- End aot ---
+//     --- Begin jit-pre-opt ---
+//     ...
+//     %{{14}}: ptr = PtrAdd %{{13}}, -{{4i32}}
+//     ...
+//     --- End jit-pre-opt ---
+//     i=9
+//     jitstate: enter-jit-code
+//     i=9
+//     i=9
+//     jitstate: deoptimise
+
+// Check that basic trace compilation works.
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new(NULL);
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  int arr[300];
+  for (int x = 0; x < 300; x++) {
+    arr[x] = x;
+  }
+
+  int i = 0;
+  int *ptr = &arr[10];
+  NOOPT_VAL(loc);
+  NOOPT_VAL(i);
+  while (i < 4) {
+    yk_mt_control_point(mt, &loc);
+    NOOPT_VAL(ptr);
+    fprintf(stderr, "i=%d\n", ptr[-1]);
+    i++;
+  }
+  yk_location_drop(loc);
+  yk_mt_drop(mt);
+  return (EXIT_SUCCESS);
+}
---
+++ tests/c/neg_ptradd_dyn_ptr.newcg.c
@@ -0,0 +1,62 @@
+// ignore-if: test $YK_JIT_COMPILER != "yk" -o "$YKB_TRACER" = "swt"
+// Run-time:
+//   env-var: YKD_LOG_IR=-:aot,jit-pre-opt
+//   env-var: YKD_SERIALISE_COMPILATION=1
+//   env-var: YKD_LOG_JITSTATE=-
+//   stderr:
+//     jitstate: start-tracing
+//     i=0, deref=9
+//     jitstate: stop-tracing
+//     --- Begin aot ---
+//     ...
+//     ${{15_3}}: ptr = PtrAdd ${{15_1}}, 0 + (${{15_2}} * {{4}})
+//     ...
+//     --- End aot ---
+//     --- Begin jit-pre-opt ---
+//     ...
+//     %{{17}}: ptr = PtrAdd %{{14}}, %{{16}}
+//     ...
+//     --- End jit-pre-opt ---
+//     i=1, deref=8
+//     jitstate: enter-jit-code
+//     i=2, deref=7
+//     i=3, deref=6
+//     jitstate: deoptimise
+
+// Check that basic trace compilation works.
+
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new(NULL);
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  int arr[300];
+  for (int x = 0; x < 300; x++) {
+    arr[x] = x;
+  }
+
+  int i = 0;
+  int *ptr = &arr[10];
+  int minus1 = -1;
+  NOOPT_VAL(loc);
+  NOOPT_VAL(i);
+  while (i < 4) {
+    yk_mt_control_point(mt, &loc);
+    NOOPT_VAL(ptr);
+    NOOPT_VAL(minus1);
+    ptr += minus1;
+    fprintf(stderr, "i=%d, deref=%" PRIu32 "\n", i, *ptr);
+    i++;
+  }
+  yk_location_drop(loc);
+  yk_mt_drop(mt);
+  return (EXIT_SUCCESS);
+}
