beed54658a78d6aeac6213f0cee59f16bbab435c
Laurence Tratt <laurie@tratt.net>
2024-05-21 12:10:57 +0100

    Add a tentative optimisation module and example optimisation.
    The aim of this commit isn't to turn the JIT compiler into a fully
    optimising compiler, but to explore what we need to do to make it
    practical to write optimisations. To do that, I've implemented a (very)
    simple dead-code elimination pass that (to keep things simple for now)
    only really works with `Add` instructions. This does work for the one
    test I've implemented, but it's (ahem) not exactly production quality!
    
    This has ended up being a more valuable exercise than I expected because
    it has immediately highlighted several factors:
    
      1. Instructions need to be `Clone` and `PartialEq`.
    
      2. For testing purposes (but not non-testing!) we need some sort of
         instruction that consumes an operand and tells (a.k.a. "lies") to
         the optimiser that the instruction has a side effect. One way of
         thinking of this is that it's equivalent to a "print instruction
         that doesn't print". I've tentatively called it `Consume` but am
         open to other naming suggestions.
    
      3. We need a way of writing human-friendly syntax for test inputs (we
         have a way -- which I haven't used -- of using human-friendly
         syntax for test outputs, but that's only half the problem). The
         "solution" to this is semi-obvious, but it's not a trivial amount
         of work.
    
      4. It's probably going to be better adding functions to instructions
         to express things like "does this instruction produce a side
         effect" rather than encoding that knowledge in every pass via
         (tediously long) `match` statements.
    
      5. We might want to think about things like adding a "rewrite operand"
         function/visitor to `Inst` rather than encoding it in each pass.
         I'm less sure about that, though.
    
    I doubt these are the only things that we'll come across as we implement
    optimisations, but it seems like a useful starting point. So this commit
    is both, to some extent, a request for comments, but also "it's probably
    worth thinking about merging this as it can be a very early guide as to
    how we get the system ready for more powerful optimisations".

---
+++ ykrt/src/compile/jitc_yk/opt/lvn.rs
@@ -0,0 +1,115 @@
+#[cfg(test)]
+use crate::compile::jitc_yk::jit_ir::TestUseInst;
+use crate::compile::{
+    jitc_yk::jit_ir::{AddInst, Inst, InstIdx, Module, Operand},
+    CompilationError,
+};
+use typed_index_collections::TiVec;
+
+/// Perform (very very) simple dead-code elimination.
+pub(super) fn lvn(mut m: Module) -> Result<Module, CompilationError> {
+    // Which instructions have an effect on the wider world?
+    let mut used_ops = vec![false; m.len()];
+    for inst in m.iter_mut_insts() {
+        match inst {
+            Inst::LoadTraceInput(_) => (),
+            Inst::Add(x) => {
+                if let Operand::Local(y) = x.lhs() {
+                    used_ops[usize::from(y)] = true;
+                }
+                if let Operand::Local(y) = x.rhs() {
+                    used_ops[usize::from(y)] = true;
+                }
+            }
+            #[cfg(test)]
+            Inst::TestUse(x) => {
+                if let Operand::Local(y) = x.operand() {
+                    used_ops[usize::from(y)] = true;
+                }
+            }
+            x => todo!("{x:?}"),
+        }
+    }
+
+    // Rewrite the sequence of instructions, eliminating instructions whose results are unused, and
+    // performing the necessary adjustments to operands in the light of those instructions being
+    // removed.
+    fn operand_off(op: Operand, offs: &[InstIdx]) -> Operand {
+        match op {
+            Operand::Local(x) => Operand::Local(offs[usize::from(x)]),
+            Operand::Const(_) => op,
+        }
+    }
+
+    let mut off = 0;
+    let mut insts = TiVec::<InstIdx, _>::new();
+    let mut offs = Vec::new();
+    for (i, inst) in m.iter_insts().enumerate() {
+        // The `unwrap` can't fail because `i - off <= i` (by definition) and `i` is a
+        // representable `InstIdx`.
+        offs.push(InstIdx::new(i - off).unwrap());
+        if !used_ops[i] {
+            match inst {
+                #[cfg(test)]
+                Inst::TestUse(_) => (),
+                Inst::Add(_) => {
+                    off += 1;
+                    continue;
+                }
+                x => todo!("{x:?}"),
+            }
+        }
+        let inst = match inst {
+            Inst::LoadTraceInput(_) => inst.clone(),
+            Inst::Add(x) => {
+                AddInst::new(operand_off(x.lhs(), &offs), operand_off(x.rhs(), &offs)).into()
+            }
+            #[cfg(test)]
+            Inst::TestUse(x) => TestUseInst::new(operand_off(x.operand(), &offs)).into(),
+            x => todo!("{x:?}"),
+        };
+        insts.push(inst);
+    }
+    m.replace_insts(insts);
+    Ok(m)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::compile::jitc_yk::jit_ir::{
+        self, AddInst, InstIdx, IntegerTy, LoadTraceInputInst, TestUseInst,
+    };
+
+    #[test]
+    fn opt_add() {
+        let mut m = Module::new_testing();
+        let i16_ty_idx = m
+            .insert_ty(jit_ir::Ty::Integer(IntegerTy::new(16)))
+            .unwrap();
+        let op1 = m
+            .push_and_make_operand(LoadTraceInputInst::new(0, i16_ty_idx).into())
+            .unwrap();
+        let op2 = m
+            .push_and_make_operand(LoadTraceInputInst::new(16, i16_ty_idx).into())
+            .unwrap();
+        let _ = m
+            .push_and_make_operand(AddInst::new(op1.clone(), op2.clone()).into())
+            .unwrap();
+        let op5 = m
+            .push_and_make_operand(AddInst::new(op1.clone(), op2.clone()).into())
+            .unwrap();
+        let op6 = m
+            .push_and_make_operand(AddInst::new(op1.clone(), op5.clone()).into())
+            .unwrap();
+        m.push(TestUseInst::new(op5).into()).unwrap();
+        m.push(TestUseInst::new(op6).into()).unwrap();
+        assert_eq!(m.len(), 7);
+        let m = lvn(m).unwrap();
+        assert_eq!(m.len(), 6);
+        assert_eq!(
+            *m.inst(InstIdx::new(3).unwrap()),
+            Into::<Inst>::into(AddInst::new(op1, Operand::Local(InstIdx::new(2).unwrap())))
+        );
+    }
+}
