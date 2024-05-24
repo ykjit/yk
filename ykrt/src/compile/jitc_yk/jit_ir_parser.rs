cfe308089b80b36e43ce40171138031da6cf2f63
Laurence Tratt <laurie@tratt.net>
2024-05-24 08:36:31 +0100

    Move the JIT IR parser into its own struct.
    It's now obvious to me that this is going to have to carry around some
    state, and passing it as function parameters each time will quickly
    become annoying.

--- ykrt/src/compile/jitc_yk/jit_ir_parser.rs
+++ ykrt/src/compile/jitc_yk/jit_ir_parser.rs
@@ -25,21 +25,7 @@ impl Module {
     ///
     /// If `s` is not parsable or otherwise does not lead to the creation of a valid `Module`.
     pub(crate) fn from_str(s: &str) -> Self {
-        // Get the `LexerDef` for the `calc` language.
-        let lexerdef = jit_ir_l::lexerdef();
-        let lexer = lexerdef.lexer(s);
-        // Pass the lexer to the parser and lex and parse the input.
-        let (res, errs) = jit_ir_y::parse(&lexer);
-        if !errs.is_empty() {
-            for e in errs {
-                eprintln!("{}", e.pp(&lexer, &jit_ir_y::token_epp));
-            }
-            panic!("Could not parse input");
-        }
-        match res {
-            Some(Ok((globals, bblocks))) => process(lexer, globals, bblocks).unwrap(),
-            _ => panic!("Could not produce JIT Module."),
-        }
+        JITIRParser::parse(s)
     }
 
     /// Assert that for the given IR input, as a string, `ir_input`, when transformed by
@@ -86,91 +72,120 @@ impl Module {
     }
 }
 
-fn process(
-    lexer: LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
-    _globals: Vec<()>,
-    bblocks: Vec<ASTBBlock>,
-) -> Result<Module, Box<dyn Error>> {
-    let mut m = Module::new_testing();
-    for bblock in bblocks {
-        for inst in bblock.insts {
-            match inst {
-                ASTInst::Add { type_: _, lhs, rhs } => {
-                    let inst =
-                        AddInst::new(process_operand(&lexer, lhs)?, process_operand(&lexer, rhs)?);
-                    m.push(inst.into()).unwrap();
-                }
-                ASTInst::LoadTraceInput { type_, off } => {
-                    let off = lexer
-                        .span_str(off)
-                        .parse::<u32>()
-                        .map_err(|e| error_at_span(&lexer, off, &e.to_string()))?;
-                    let inst = LoadTraceInputInst::new(off, process_type(&lexer, &mut m, type_)?);
-                    m.push(inst.into()).unwrap();
-                }
-                ASTInst::TestUse(op) => {
-                    let inst = TestUseInst::new(process_operand(&lexer, op)?);
-                    m.push(inst.into()).unwrap();
+struct JITIRParser;
+
+impl JITIRParser {
+    fn parse(s: &str) -> Module {
+        // Get the `LexerDef` for the `calc` language.
+        let lexerdef = jit_ir_l::lexerdef();
+        let lexer = lexerdef.lexer(s);
+        // Pass the lexer to the parser and lex and parse the input.
+        let (res, errs) = jit_ir_y::parse(&lexer);
+        if !errs.is_empty() {
+            for e in errs {
+                eprintln!("{}", e.pp(&lexer, &jit_ir_y::token_epp));
+            }
+            panic!("Could not parse input");
+        }
+        match res {
+            Some(Ok((globals, bblocks))) => Self.process(lexer, globals, bblocks).unwrap(),
+            _ => panic!("Could not produce JIT Module."),
+        }
+    }
+
+    fn process(
+        &self,
+        lexer: LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
+        _globals: Vec<()>,
+        bblocks: Vec<ASTBBlock>,
+    ) -> Result<Module, Box<dyn Error>> {
+        let mut m = Module::new_testing();
+        for bblock in bblocks {
+            for inst in bblock.insts {
+                match inst {
+                    ASTInst::Add { type_: _, lhs, rhs } => {
+                        let inst = AddInst::new(
+                            self.process_operand(&lexer, lhs)?,
+                            self.process_operand(&lexer, rhs)?,
+                        );
+                        m.push(inst.into()).unwrap();
+                    }
+                    ASTInst::LoadTraceInput { type_, off } => {
+                        let off = lexer
+                            .span_str(off)
+                            .parse::<u32>()
+                            .map_err(|e| self.error_at_span(&lexer, off, &e.to_string()))?;
+                        let inst =
+                            LoadTraceInputInst::new(off, self.process_type(&lexer, &mut m, type_)?);
+                        m.push(inst.into()).unwrap();
+                    }
+                    ASTInst::TestUse(op) => {
+                        let inst = TestUseInst::new(self.process_operand(&lexer, op)?);
+                        m.push(inst.into()).unwrap();
+                    }
                 }
             }
         }
+        Ok(m)
     }
-    Ok(m)
-}
 
-fn process_operand(
-    lexer: &LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
-    op: ASTOperand,
-) -> Result<Operand, Box<dyn Error>> {
-    match op {
-        ASTOperand::Local(span) => {
-            let idx = lexer.span_str(span)[1..]
-                .parse::<usize>()
-                .map_err(|e| error_at_span(lexer, span, &e.to_string()))?;
-            Ok(Operand::Local(
-                InstIdx::new(idx).map_err(|e| error_at_span(lexer, span, &e.to_string()))?,
-            ))
+    fn process_operand(
+        &self,
+        lexer: &LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
+        op: ASTOperand,
+    ) -> Result<Operand, Box<dyn Error>> {
+        match op {
+            ASTOperand::Local(span) => {
+                let idx = lexer.span_str(span)[1..]
+                    .parse::<usize>()
+                    .map_err(|e| self.error_at_span(lexer, span, &e.to_string()))?;
+                Ok(Operand::Local(InstIdx::new(idx).map_err(|e| {
+                    self.error_at_span(lexer, span, &e.to_string())
+                })?))
+            }
         }
     }
-}
 
-fn process_type(
-    lexer: &LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
-    m: &mut Module,
-    type_: ASTType,
-) -> Result<TyIdx, Box<dyn Error>> {
-    match type_ {
-        ASTType::Int(span) => {
-            let width = lexer.span_str(span)[1..]
-                .parse::<u32>()
-                .map_err(|e| error_at_span(lexer, span, &e.to_string()))?;
-            let ty = IntegerTy::new(width);
-            m.insert_ty(Ty::Integer(ty))
-                .map_err(|e| error_at_span(lexer, span, &e.to_string()))
+    fn process_type(
+        &self,
+        lexer: &LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
+        m: &mut Module,
+        type_: ASTType,
+    ) -> Result<TyIdx, Box<dyn Error>> {
+        match type_ {
+            ASTType::Int(span) => {
+                let width = lexer.span_str(span)[1..]
+                    .parse::<u32>()
+                    .map_err(|e| self.error_at_span(lexer, span, &e.to_string()))?;
+                let ty = IntegerTy::new(width);
+                m.insert_ty(Ty::Integer(ty))
+                    .map_err(|e| self.error_at_span(lexer, span, &e.to_string()))
+            }
         }
     }
-}
 
-/// Return an error message pinpointing `span` as the culprit.
-fn error_at_span(
-    lexer: &LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
-    span: Span,
-    msg: &str,
-) -> Box<dyn Error> {
-    let ((line_off, col), _) = lexer.line_col(span);
-    let code = lexer
-        .span_lines_str(span)
-        .split('\n')
-        .next()
-        .unwrap()
-        .trim();
-    Box::from(format!(
-        "Line {}, column {}:\n  {}\n{}",
-        line_off,
-        col,
-        code.trim(),
-        msg
-    ))
+    /// Return an error message pinpointing `span` as the culprit.
+    fn error_at_span(
+        &self,
+        lexer: &LRNonStreamingLexer<DefaultLexerTypes<StorageT>>,
+        span: Span,
+        msg: &str,
+    ) -> Box<dyn Error> {
+        let ((line_off, col), _) = lexer.line_col(span);
+        let code = lexer
+            .span_lines_str(span)
+            .split('\n')
+            .next()
+            .unwrap()
+            .trim();
+        Box::from(format!(
+            "Line {}, column {}:\n  {}\n{}",
+            line_off,
+            col,
+            code.trim(),
+            msg
+        ))
+    }
 }
 
 #[derive(Debug)]
