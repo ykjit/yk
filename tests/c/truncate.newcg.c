43c610ebcbb32afbf262f41b88b2148bcff6b6f1
Edd Barrett <vext01@gmail.com>
2024-07-22 16:22:48 +0100

    Adjust comment prefix in lang_tester.
    This will let us match debuginfo later.

--- tests/c/truncate.newcg.c
+++ tests/c/truncate.newcg.c
@@ -1,6 +1,6 @@
-// # Truncation currently always truncates to i32, no matter the target type.
-// #ignore-if: true
-// # ignore-if: test $YK_JIT_COMPILER != "yk" -o "$YKB_TRACER" = "swt"
+// ## Truncation currently always truncates to i32, no matter the target type.
+// ##ignore-if: true
+// ## ignore-if: test $YK_JIT_COMPILER != "yk" -o "$YKB_TRACER" = "swt"
 // Run-time:
 //   env-var: YKD_SERIALISE_COMPILATION=1
 //   env-var: YKD_LOG_JITSTATE=-
