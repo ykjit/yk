8a48dc188e4c9adfd894f9d40c9cd7b6503ecf5c
Laurence Tratt <laurie@tratt.net>
2024-07-29 16:25:43 +0100

    These Cargo features have been disabled, so fully remove them.
    They were partly removed in d127bba195625271e0bc3bcdbe219cb75cc5a116;
    this commit finishes the job off.

--- ykcapi/build.rs
+++ ykcapi/build.rs
@@ -3,10 +3,6 @@ use std::env;
 pub fn main() {
     ykbuild::apply_llvm_ld_library_path();
 
-    // FIXME: This is a temporary hack because LLVM has problems if the main thread exits before
-    // compilation threads have finished.
-    println!("cargo:rustc-cfg=yk_llvm_sync_hack");
-
     println!("cargo:rerun-if-env-changed=YKB_TRACER");
     match env::var("YKB_TRACER") {
         Ok(ref tracer) if tracer == "swt" => println!("cargo:rustc-cfg=tracer_swt"),
