cfc3f9a4a245003171108f1870c4771e3803127a
Edd Barrett <vext01@gmail.com>
2024-08-09 15:38:38 +0100

    YK_JIT_COMPILER is no more.
    For now we have only one JIT compiler and I can't see anywhere the
    system sets/reads `YK_JIT_COMPILER`, so we may as well kill all uses of
    it.

--- tests/c/icmp_ptr.c
+++ tests/c/icmp_ptr.c
@@ -1,4 +1,4 @@
-// ignore-if: test $YK_JIT_COMPILER != "yk" -o "$YKB_TRACER" = "swt"
+// ignore-if: test "$YKB_TRACER" = "swt"
 // Run-time:
 //   env-var: YKD_SERIALISE_COMPILATION=1
 //   env-var: YK_LOG=4
