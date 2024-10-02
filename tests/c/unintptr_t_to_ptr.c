d8123a9f7fbee005c48c587f1d62c10ac7c00136
Edd Barrett <vext01@gmail.com>
2024-10-02 11:21:21 +0100

    Relax cg_zeroextend to allow zeroextends into pointer types.
    This is required for non-truncating LLVM `inttoptr` instructions that
    get lowered to Yk zeroextend instructions.
    
    (This routine has already been relaxed the opposing `ptrtoint` case)

---
+++ tests/c/unintptr_t_to_ptr.c
@@ -0,0 +1,45 @@
+// Run-time:
+//   env-var: YKD_LOG_IR=-:aot
+//   env-var: YKD_SERIALISE_COMPILATION=1
+//   env-var: YK_LOG=4
+//   stderr:
+//     yk-jit-event: start-tracing
+//     4: 0x4
+//     yk-jit-event: stop-tracing
+//     --- Begin aot ---
+//     ...
+//     %{{11_2}}: ptr = zext %{{11_1}}, ptr
+//     %{{11_3}}: i32 = call fprintf(%{{_}}, @{{_}}, %{{11_1}}, %{{11_2}})
+//     ...
+//     --- End aot ---
+//     3: 0x3
+//     yk-jit-event: enter-jit-code
+//     2: 0x2
+//     1: 0x1
+//     yk-jit-event: deoptimise
+
+// Check that converting an integer to a pointer works.
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <yk.h>
+#include <yk_testing.h>
+
+int main(int argc, char **argv) {
+  YkMT *mt = yk_mt_new(NULL);
+  yk_mt_hot_threshold_set(mt, 0);
+  YkLocation loc = yk_location_new();
+
+  uintptr_t i = 4;
+  NOOPT_VAL(loc);
+  NOOPT_VAL(i);
+  while (i > 0) {
+    yk_mt_control_point(mt, &loc);
+    fprintf(stderr, "%" PRIuPTR ": %p\n", i, (void *) i);
+    i--;
+  }
+  yk_location_drop(loc);
+  yk_mt_shutdown(mt);
+  return (EXIT_SUCCESS);
+}
