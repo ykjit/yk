ee773ad210c097c05774f98506cb41476f4b2747
Laurence Tratt <laurie@tratt.net>
2025-02-05 16:13:42 +0000

    Implement codegen for 64 bit llvm.smax.
    This is effectively an IR intrinsic, but is encoded as a call.

--- ykrt/src/compile/jitc_yk/codegen/x64/mod.rs
+++ ykrt/src/compile/jitc_yk/codegen/x64/mod.rs
@@ -1618,6 +1618,11 @@ impl<'a> Assemble<'a> {
             "llvm.assume" => Ok(()),
             "llvm.lifetime.start.p0" => Ok(()),
             "llvm.lifetime.end.p0" => Ok(()),
+            x if x.starts_with("llvm.smax") => {
+                let [lhs_op, rhs_op] = args.try_into().unwrap();
+                self.cg_smax(iidx, lhs_op, rhs_op);
+                Ok(())
+            }
             x => {
                 let va = symbol_to_ptr(x).map_err(|e| CompilationError::General(e.to_string()))?;
                 self.emit_call(iidx, fty, Some(va), None, &args)
@@ -1788,6 +1793,38 @@ impl<'a> Assemble<'a> {
         }
 
         Ok(())
+    }
+
+    fn cg_smax(&mut self, iidx: InstIdx, lhs: Operand, rhs: Operand) {
+        assert_eq!(lhs.bitw(self.m), rhs.bitw(self.m));
+        let bitw = lhs.bitw(self.m);
+        let [lhs_reg, rhs_reg] = self.ra.assign_gp_regs(
+            &mut self.asm,
+            iidx,
+            [
+                GPConstraint::InputOutput {
+                    op: lhs,
+                    in_ext: RegExtension::SignExtended,
+                    out_ext: RegExtension::SignExtended,
+                    force_reg: None,
+                },
+                GPConstraint::Input {
+                    op: rhs,
+                    in_ext: RegExtension::SignExtended,
+                    force_reg: None,
+                    clobber_reg: false,
+                },
+            ],
+        );
+        match bitw {
+            64 => {
+                dynasm!(self.asm
+                    ; cmp Rq(lhs_reg.code()), Rq(rhs_reg.code())
+                    ; cmovl Rq(lhs_reg.code()), Rq(rhs_reg.code())
+                );
+            }
+            x => todo!("{x}"),
+        }
     }
 
     /// Return the [VarLocation] an [Operand] relates to.
@@ -4043,6 +4080,27 @@ mod tests {
                ; call @llvm.lifetime.end.p0(16i64, %1)
                ; %5: ...
                ...
+            ",
+            false,
+        );
+    }
+
+    #[test]
+    fn cg_call_smax() {
+        codegen_and_test(
+            "
+             func_decl llvm.smax.i64 (i64, i64) -> i64
+             entry:
+               %0: i64 = param 0
+               %1: i64 = param 1
+               %2: i64 = call @llvm.smax.i64(%0, %1)
+               black_box %2
+            ",
+            "
+               ...
+               ; %2: i64 = call @llvm.smax.i64(%0, %1)
+               cmp r.64.a, r.64.b
+               cmovl r.64.a, r.64.b
             ",
             false,
         );
