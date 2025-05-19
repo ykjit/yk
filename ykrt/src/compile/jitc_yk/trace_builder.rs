91bfe8f8fa105f15e8dbc1b1d7e913a7a538fa5c
Laurence Tratt <laurie@tratt.net>
2025-05-19 08:57:23 +0100

    Remove incorrect comment.

--- ykrt/src/compile/jitc_yk/trace_builder.rs
+++ ykrt/src/compile/jitc_yk/trace_builder.rs
@@ -1276,8 +1276,7 @@ impl<Register: Send + Sync + 'static> TraceBuilder<Register> {
         // FIXME: This is a workaround so we can peek at the last block in a trace in order to
         // extract information from the control point when we are side-tracing. Ideally, we extract
         // this information at AOT compile time and serialise it into the module (or block), so we
-        // don't have to do this. Note, we also can't use `collect` here since that won't catch the
-        // `TraceTooLong` error early enough and we run out of memory.
+        // don't have to do this.
         mt.stats.timing_state(TimingState::TraceMapping);
         let tas = ta_iter
             .map(|x| {
