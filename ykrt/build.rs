0e3b3ff8868342064c683285f12817dcd8855a20
Laurence Tratt <laurie@tratt.net>
2025-11-03 12:25:20 +0000

    Remove compile-time guards.
    These are always, and will almost certainly always be, on -- until we
    remove a backend at which point we'll remove all the relevant code.
    
    At the moment their only practical effect is to confuse rust-analyzer
    (causing it to sometimes stop completing things in one other subsystem),
    which isn't a price that seems worth paying.

--- ykrt/build.rs
+++ ykrt/build.rs
@@ -10,11 +10,6 @@ use {
 
 pub fn main() {
     println!("cargo::rerun-if-env-changed=YKB_TRACER");
-    // Always compile in yk's default JIT compiler.
-    println!("cargo::rustc-cfg=jitc_yk");
-    println!("cargo::rustc-check-cfg=cfg(jitc_yk)");
-    println!("cargo::rustc-cfg=jitc_j2");
-    println!("cargo::rustc-check-cfg=cfg(jitc_j2)");
 
     println!("cargo::rustc-check-cfg=cfg(tracer_hwt)");
     println!("cargo::rustc-check-cfg=cfg(tracer_swt)");
