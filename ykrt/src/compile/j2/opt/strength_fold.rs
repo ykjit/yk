4a6d545d9113aa727a8caf9ea2d3bca03afda8e1
Laurence Tratt <laurie@tratt.net>
2025-12-08 14:50:47 +0000

    Optimise `sub`.

--- ykrt/src/compile/j2/opt/strength_fold.rs
+++ ykrt/src/compile/j2/opt/strength_fold.rs
@@ -13,6 +13,7 @@ pub(super) fn strength_fold(opt: &mut Opt, inst: Inst) -> OptOutcome {
         Inst::Add(x) => opt_add(opt, x),
         Inst::And(x) => opt_and(opt, x),
         Inst::Guard(x) => opt_guard(opt, x),
+        Inst::Sub(x) => opt_sub(opt, x),
         _ => OptOutcome::Rewritten(inst),
     }
 }
@@ -126,6 +127,46 @@ fn opt_guard(opt: &mut Opt, inst @ Guard { expect, cond, .. }: Guard) -> OptOutc
             assert!(!samesign);
             opt.set_range(lhs, Range::Equivalent(rhs));
         }
+    }
+
+    OptOutcome::Rewritten(inst.into())
+}
+
+fn opt_sub(
+    opt: &mut Opt,
+    inst @ Sub {
+        tyidx,
+        lhs,
+        rhs,
+        nuw,
+        nsw,
+    }: Sub,
+) -> OptOutcome {
+    assert!(!nuw && !nsw);
+    if let (
+        Inst::Const(Const {
+            kind: ConstKind::Int(lhs_c),
+            ..
+        }),
+        Inst::Const(Const {
+            kind: ConstKind::Int(rhs_c),
+            ..
+        }),
+    ) = (opt.inst_rewrite(lhs), opt.inst_rewrite(rhs))
+    {
+        // Constant fold `c1 - c2`.
+        return OptOutcome::Rewritten(Inst::Const(Const {
+            tyidx,
+            kind: ConstKind::Int(lhs_c.wrapping_sub(&rhs_c)),
+        }));
+    } else if let Inst::Const(Const {
+        kind: ConstKind::Int(rhs_c),
+        ..
+    }) = opt.inst_rewrite(rhs)
+        && rhs_c.to_zero_ext_u8() == Some(0)
+    {
+        // Reduce `x - 0` to `x`.
+        return OptOutcome::ReducedTo(lhs);
     }
 
     OptOutcome::Rewritten(inst.into())
@@ -381,6 +422,55 @@ mod test {
           blackbox %3
           blackbox %3
           exit [%3, %3]
+        ",
+        );
+    }
+
+    #[test]
+    fn opt_sub() {
+        // Simple constant folding e.g `1 - 2`.
+        test_sf(
+            "
+          %0: i8 = 3
+          %1: i8 = 2
+          %2: i8 = sub %0, %1
+          blackbox %2
+        ",
+            "
+          ...
+          %2: i8 = 1
+          blackbox %2
+        ",
+        );
+
+        // Test constant folding wraps.
+        test_sf(
+            "
+          %0: i8 = 2
+          %1: i8 = 4
+          %2: i8 = sub %0, %1
+          blackbox %2
+        ",
+            "
+          ...
+          %2: i8 = 254
+          blackbox %2
+        ",
+        );
+
+        // Strength reduction of `x - 0`.
+        test_sf(
+            "
+          %0: i8 = arg [ reg ]
+          %1: i8 = 0
+          %2: i8 = sub %0, %1
+          exit [%2]
+        ",
+            "
+          ...
+          %0: i8 = arg
+          %1: i8 = 0
+          exit [%0]
         ",
         );
     }
