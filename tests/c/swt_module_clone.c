83ccf17c5ed4581779430ac5fe295e7b893475f4
Edd Barrett <vext01@gmail.com>
2026-02-17 15:37:08 +0000

    Remove a few remaining YK_JITC guards from tests.
    We keep those that set `YK_JITC=none` though.

--- tests/c/swt_module_clone.c
+++ tests/c/swt_module_clone.c
@@ -1,4 +1,4 @@
-// ignore-if: test "$YKB_TRACER" != "swt" -o "$YK_JITC" != "j2"
+// ignore-if: test "$YKB_TRACER" != "swt"
 // Run-time:
 //   env-var: YKD_LOG_IR=aot,jit-pre-opt
 //   env-var: YKD_SERIALISE_COMPILATION=1
