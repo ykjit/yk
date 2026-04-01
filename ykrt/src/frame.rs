a3ca4cf407fe0de03f67a241e68053b6895b7493
Laurence Tratt <laurie@tratt.net>
2026-04-01 11:28:51 +0100

    Fix comment.

--- ykrt/src/frame.rs
+++ ykrt/src/frame.rs
@@ -1,6 +1,6 @@
 use std::ffi::c_void;
 
-/// Return `true` if the frame at address `current` must be a caller of `previous`.
+/// Return `true` if the frame at address `cnd_caller` is definitely a caller of `cnd_callee`.
 #[cfg(target_arch = "x86_64")]
 pub(crate) fn is_caller_frame(cnd_caller: *const c_void, cnd_callee: *const c_void) -> bool {
     cnd_callee < cnd_caller
