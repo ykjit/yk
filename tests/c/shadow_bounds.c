6d6bcb11793284cca5a91aa49a276c9985a45f33
Pavel Durov <pavel@p3ld3v.dev>
2026-05-16 11:47:28 +0100

    Add tracing-state check handling for yk_indirect_inline functions.
    yk_indirect_inline functions have a tracing-state check prepended at
    by ModuleClone. During tracing the state is always non-zero so we
    convert these to constants.

--- tests/c/shadow_bounds.c
+++ tests/c/shadow_bounds.c
@@ -10,6 +10,7 @@
 //     ...onshadow=1
 
 // Check we can get the shadow stack bounds for the current thread.
+// The control point in `f` allocates a shadow stack for each thread.
 
 #include <assert.h>
 #include <err.h>
@@ -21,33 +22,41 @@
 
 #define NUM_THREADS 8
 
-// `yk_indirect_inline` is to stop this being marked as untraceable, thus
-// ensuring it gets a shadow stack.
-__attribute__((yk_indirect_inline))
 void *f(void *arg) {
+  YkMT *mt = (YkMT *)arg;
+  YkLocation loc = yk_location_new();
+  // yk_mt_control_point requires to be in a loop.
   int x = 0;
-  NOOPT_VAL(x);
-  void *ss_start = NULL, *ss_end = NULL;
-  yk_thread_shadowstack_bounds(&ss_start, &ss_end);
-  int inbounds = ((void *) &x >= ss_start) && ((void *) &x <= ss_end);
-  fprintf(stderr, "&x=%p, ss=[%p, %p], onshadow=%d\n", &x, ss_start, ss_end, inbounds);
-  assert(ss_start < ss_end);
-  assert(inbounds);
+  while(x == 0) {
+    NOOPT_VAL(x);
+    // Control point allocates the shadow stack for this thread.
+    yk_mt_control_point(mt, &loc);
+    void *ss_start = NULL, *ss_end = NULL;
+    yk_thread_shadowstack_bounds(&ss_start, &ss_end);
+    int inbounds = ((void *)&x >= ss_start) && ((void *)&x <= ss_end);
+    fprintf(stderr, "&x=%p, ss=[%p, %p], onshadow=%d\n", &x, ss_start, ss_end,
+            inbounds);
+    assert(ss_start < ss_end);
+    assert(inbounds);
+    x = 1;
+  }
   return NULL;
 }
 
 int main() {
+  YkMT *mt = yk_mt_new(NULL);
   pthread_t threads[NUM_THREADS];
   for (int i = 0; i < NUM_THREADS; i++) {
-    if (pthread_create(&threads[i], NULL, f, NULL) != 0)
+    if (pthread_create(&threads[i], NULL, f, mt) != 0)
       err(EXIT_FAILURE, "pthread_create");
   }
 
-  void *thread_res = 0;
+  void *thread_res = NULL;
   for (int i = 0; i < NUM_THREADS; i++) {
     if (pthread_join(threads[i], &thread_res) != 0)
       err(EXIT_FAILURE, "pthread_join");
   }
 
+  yk_mt_shutdown(mt);
   return (EXIT_SUCCESS);
 }
