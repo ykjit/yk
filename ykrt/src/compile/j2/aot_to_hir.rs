1b4b6ad14a17bb9b25fba52bc6f05d6c500e88bd
Pavel Durov <pavel@p3ld3v.dev>
2026-08-14 10:31:27 +0100

    Rename aggregate_iidx to val_iidx.

--- ykrt/src/compile/j2/aot_to_hir.rs
+++ ykrt/src/compile/j2/aot_to_hir.rs
@@ -1872,15 +1872,15 @@ impl<'a, Reg: RegT + 'static> AotToHir<'a, Reg> {
         };
 
         if let Operand::Local(aot_iid) = op {
-            let aggregate_iidx = self.frames.last().unwrap().get_local(&*self.opt, aot_iid);
-            if let hir::Inst::Call(_) = self.opt.inst(aggregate_iidx) {
+            let val_iidx = self.frames.last().unwrap().get_local(&*self.opt, aot_iid);
+            if let hir::Inst::Call(_) = self.opt.inst(val_iidx) {
                 let off = u32::try_from(struct_ty.field_bit_offs()[indices[0]]).unwrap();
                 let res_tyidx = self.p_ty(self.am.type_(*tyidx))?;
                 return self
                     .push_inst_and_link_local(
                         iid,
                         hir::ExtractVal {
-                            val: aggregate_iidx,
+                            val: val_iidx,
                             off,
                             tyidx: res_tyidx,
                         },
