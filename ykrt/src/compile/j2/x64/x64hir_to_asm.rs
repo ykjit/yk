614ac8945029c53e04df60ac1748f8456f6cf40d
Laurence Tratt <laurie@tratt.net>
2026-09-23 15:40:22 +0100

    Fold loads into `i_binop`.
    As elsewhere, we face the tension of "should we fold loads in at all
    times or only if the load's result isn't used?" I still don't know what
    the best general policy is (there might not even be one).

--- ykrt/src/compile/j2/x64/x64hir_to_asm.rs
+++ ykrt/src/compile/j2/x64/x64hir_to_asm.rs
@@ -331,11 +331,13 @@ impl<'a> X64HirToAsm<'a> {
     }
 
     /// A generic function for binary operations (add/sub/etc). `nonload` is a function which must
-    /// return the [IcedInst] when `rhs` is not a load.
+    /// return the [IcedInst] when `rhs` is not a load; `load` is a function for when `rhs` is a
+    /// `load` which can be folded in.
     ///
     /// Note: this function can't, and doesn't try to, handle every permutation of x64: there are
-    /// some binary operations that aren't a good fit for it.
-    fn i_binop<F, G>(
+    /// some binary operations that aren't a good fit for it. It also assumes that input values
+    /// have an `Undefined` register fill.
+    fn i_binop<F, G, H>(
         &mut self,
         ra: &mut RegAlloc<Self>,
         b: &Block,
@@ -344,31 +346,52 @@ impl<'a> X64HirToAsm<'a> {
         rhs: InstIdx,
         out_fill: F,
         rhs_nonload: G,
+        rhs_load: H,
     ) -> Result<(), CompilationError>
     where
         F: Fn(u32) -> RegCnstrFill,
         G: Fn(u32, Reg, Reg) -> Result<IcedInst, iced_x86::IcedError>,
+        H: Fn(u32, Reg, MemoryOperand) -> Result<IcedInst, iced_x86::IcedError>,
     {
         let bitw = b.inst_bitw(self.m, lhs);
-        let [lhsr, rhsr] = ra.alloc(
-            self,
-            iidx,
-            [
+        if !ra.is_used(rhs)
+            && self.try_load_to_mem_op(b, iidx, rhs).is_some()
+            && let Inst::Load(Load { ptr, .. }) = b.inst(rhs)
+        {
+            let (memop, lhsr, _) = self.alloc_mem_op_with_reg(
+                ra,
+                b,
+                iidx,
+                *ptr,
                 RegCnstr::InputOutput {
                     in_iidx: lhs,
                     in_fill: RegCnstrFill::Undefined,
                     out_fill: out_fill(bitw),
                     regs: &NORMAL_GP_REGS,
                 },
-                RegCnstr::Input {
-                    in_iidx: rhs,
-                    in_fill: RegCnstrFill::Undefined,
-                    regs: &NORMAL_GP_REGS,
-                    clobber: false,
-                },
-            ],
-        )?;
-        self.asm.push_inst(rhs_nonload(bitw, lhsr, rhsr));
+            )?;
+            self.asm.push_inst(rhs_load(bitw, lhsr, memop));
+        } else {
+            let [lhsr, rhsr] = ra.alloc(
+                self,
+                iidx,
+                [
+                    RegCnstr::InputOutput {
+                        in_iidx: lhs,
+                        in_fill: RegCnstrFill::Undefined,
+                        out_fill: out_fill(bitw),
+                        regs: &NORMAL_GP_REGS,
+                    },
+                    RegCnstr::Input {
+                        in_iidx: rhs,
+                        in_fill: RegCnstrFill::Undefined,
+                        regs: &NORMAL_GP_REGS,
+                        clobber: false,
+                    },
+                ],
+            )?;
+            self.asm.push_inst(rhs_nonload(bitw, lhsr, rhsr));
+        }
         Ok(())
     }
 
@@ -2295,6 +2318,11 @@ impl HirToAsmBackend for X64HirToAsm<'_> {
                     64 => IcedInst::with2(Code::Add_rm64_r64, lhsr.to_reg64(), rhsr.to_reg64()),
                     x => todo!("{x}"),
                 },
+                |bitw, lhsr, rhsmemop| match bitw {
+                    1..=32 => IcedInst::with2(Code::Add_r32_rm32, lhsr.to_reg32(), rhsmemop),
+                    64 => IcedInst::with2(Code::Add_r64_rm64, lhsr.to_reg64(), rhsmemop),
+                    x => todo!("{x}"),
+                },
             )?;
         }
 
@@ -2365,6 +2393,11 @@ impl HirToAsmBackend for X64HirToAsm<'_> {
                 |bitw, lhsr, rhsr| match bitw {
                     1..=32 => IcedInst::with2(Code::And_rm32_r32, lhsr.to_reg32(), rhsr.to_reg32()),
                     64 => IcedInst::with2(Code::And_rm64_r64, lhsr.to_reg64(), rhsr.to_reg64()),
+                    x => todo!("{x}"),
+                },
+                |bitw, lhsr, rhsmemop| match bitw {
+                    1..=32 => IcedInst::with2(Code::And_r32_rm32, lhsr.to_reg32(), rhsmemop),
+                    64 => IcedInst::with2(Code::And_r64_rm64, lhsr.to_reg64(), rhsmemop),
                     x => todo!("{x}"),
                 },
             )?;
@@ -4138,6 +4171,11 @@ impl HirToAsmBackend for X64HirToAsm<'_> {
                     64 => IcedInst::with2(Code::Or_rm64_r64, lhsr.to_reg64(), rhsr.to_reg64()),
                     x => todo!("{x}"),
                 },
+                |bitw, lhsr, rhsmemop| match bitw {
+                    1..=32 => IcedInst::with2(Code::Or_r32_rm32, lhsr.to_reg32(), rhsmemop),
+                    64 => IcedInst::with2(Code::Or_r64_rm64, lhsr.to_reg64(), rhsmemop),
+                    x => todo!("{x}"),
+                },
             )?;
         }
 
@@ -4806,6 +4844,11 @@ impl HirToAsmBackend for X64HirToAsm<'_> {
                     64 => IcedInst::with2(Code::Sub_rm64_r64, lhsr.to_reg64(), rhsr.to_reg64()),
                     x => todo!("{x}"),
                 },
+                |bitw, lhsr, rhsmemop| match bitw {
+                    32 => IcedInst::with2(Code::Sub_r32_rm32, lhsr.to_reg32(), rhsmemop),
+                    64 => IcedInst::with2(Code::Sub_r64_rm64, lhsr.to_reg64(), rhsmemop),
+                    x => todo!("{x}"),
+                },
             )?;
         }
         Ok(())
@@ -5159,6 +5202,11 @@ impl HirToAsmBackend for X64HirToAsm<'_> {
                 |bitw, lhsr, rhsr| match bitw {
                     1..=32 => IcedInst::with2(Code::Xor_rm32_r32, lhsr.to_reg32(), rhsr.to_reg32()),
                     64 => IcedInst::with2(Code::Xor_rm64_r64, lhsr.to_reg64(), rhsr.to_reg64()),
+                    x => todo!("{x}"),
+                },
+                |bitw, lhsr, rhsmemop| match bitw {
+                    1..=32 => IcedInst::with2(Code::Xor_r32_rm32, lhsr.to_reg32(), rhsmemop),
+                    64 => IcedInst::with2(Code::Xor_r64_rm64, lhsr.to_reg64(), rhsmemop),
                     x => todo!("{x}"),
                 },
             )?;
@@ -6096,6 +6144,25 @@ mod test {
               add r.32.x, 0x20
               ...
             "],
+        );
+
+        codegen_and_test(
+            "
+              %0: i8 = arg [reg]
+              %1: ptr = arg [reg]
+              %2: i8 = load %1
+              %3: i8 = add %0, %2
+              term [%3, %1]
+            ",
+            &[r#"
+              ...
+              ; %0: i8 = arg [Reg("r.64.x", Undefined)]
+              ; %1: ptr = arg [Reg("r.64.y", Undefined)]
+              ; %2: i8 = load %1
+              ; %3: i8 = add %0, %2
+              add r.32.x, [r.64.y]
+              ; term [%3, %1]
+            "#],
         );
 
         // i32
@@ -9715,6 +9782,26 @@ mod test {
               ...
             "],
         );
+
+        // Load folding
+        codegen_and_test(
+            "
+              %0: i8 = arg [reg]
+              %1: ptr = arg [reg]
+              %2: i8 = load %1
+              %3: i8 = or %0, %2
+              term [%3, %1]
+            ",
+            &[r#"
+              ...
+              ; %0: i8 = arg [Reg("r.64.x", Undefined)]
+              ; %1: ptr = arg [Reg("r.64.y", Undefined)]
+              ; %2: i8 = load %1
+              ; %3: i8 = or %0, %2
+              or r.32.x, [r.64.y]
+              ; term [%3, %1]
+            "#],
+        );
     }
 
     #[test]
@@ -11148,6 +11235,25 @@ mod test {
             "],
         );
 
+        codegen_and_test(
+            "
+              %0: i32 = arg [reg]
+              %1: ptr = arg [reg]
+              %2: i32 = load %1
+              %3: i32 = sub %0, %2
+              term [%3, %1]
+            ",
+            &[r#"
+              ...
+              ; %0: i32 = arg [Reg("r.64.x", Undefined)]
+              ; %1: ptr = arg [Reg("r.64.y", Undefined)]
+              ; %2: i32 = load %1
+              ; %3: i32 = sub %0, %2
+              sub r.32.x, [r.64.y]
+              ; term [%3, %1]
+            "#],
+        );
+
         // i64
         codegen_and_test(
             "
@@ -11682,6 +11788,26 @@ mod test {
               xor r.64._, r.64._
               ...
             "],
+        );
+
+        // Load folding
+        codegen_and_test(
+            "
+              %0: i8 = arg [reg]
+              %1: ptr = arg [reg]
+              %2: i8 = load %1
+              %3: i8 = xor %0, %2
+              term [%3, %1]
+            ",
+            &[r#"
+              ...
+              ; %0: i8 = arg [Reg("r.64.x", Undefined)]
+              ; %1: ptr = arg [Reg("r.64.y", Undefined)]
+              ; %2: i8 = load %1
+              ; %3: i8 = xor %0, %2
+              xor r.32.x, [r.64.y]
+              ; term [%3, %1]
+            "#],
         );
     }
 
